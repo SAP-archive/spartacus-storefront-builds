c4c9656be45fdb79d672905a544ea560d3ed1628
GitHub <noreply@github.com>
2019-05-06 15:25:34 +0000

    df3f4e99b refactor(core,storefront): drop support of loading routes config from backend, and simplify routes config (#2086)

--- lib/storefront-config.d.ts
+++ lib/storefront-config.d.ts
@@ -1,5 +1,5 @@
-import { AuthConfig, CmsConfig, ConfigurableRoutesConfig, I18nConfig, OccConfig, PersonalizationConfig, SiteContextConfig, StateConfig } from '@spartacus/core';
+import { AuthConfig, CmsConfig, I18nConfig, OccConfig, RoutingConfig, PersonalizationConfig, SiteContextConfig, StateConfig } from '@spartacus/core';
 import { IconConfig } from '../cms-components/misc/icon/index';
 import { PWAModuleConfig } from '../cms-structure/pwa/index';
 import { LayoutConfig } from '../layout/config/layout-config';
-export declare type StorefrontModuleConfig = AuthConfig | CmsConfig | OccConfig | StateConfig | PWAModuleConfig | SiteContextConfig | LayoutConfig | ConfigurableRoutesConfig | I18nConfig | PersonalizationConfig | IconConfig;
+export declare type StorefrontModuleConfig = AuthConfig | CmsConfig | OccConfig | StateConfig | PWAModuleConfig | SiteContextConfig | LayoutConfig | RoutingConfig | I18nConfig | PersonalizationConfig | IconConfig;
