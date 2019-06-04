a378fc7b4a92a4d781f34996891149c2a2e9f6f5
GitHub <noreply@github.com>
2019-06-04 15:36:40 +0000

    dd49e0c7c refactor: Adjusted outlets on PDP

--- cms-components/product/product-outlets.model.d.ts
+++ cms-components/product/product-outlets.model.d.ts
@@ -1,14 +1,8 @@
 export declare enum ProductDetailOutlets {
     SUMMARY = "PDP.SUMMARY",
-    IMAGES = "PDP.IMAGES",
     TITLE = "PDP.TITLE",
     RATING = "PDP.RATING",
     PRICE = "PDP.PRICE",
-    SHARE = "PDP.SHARE"
-}
-export declare enum ProductTabsOutlets {
-    DESCRIPTION = "PDP.DESCRIPTION",
-    SPECIFICATIONS = "PDP.SPECIFICATIONS",
-    REVIEWS = "PDP.REVIEWS",
-    SHIPPING = "PDP.SHIPPING"
+    SHARE = "PDP.SHARE",
+    DESCRIPTION = "PDP.DESCRIPTION"
 }
