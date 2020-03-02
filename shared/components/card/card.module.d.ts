ca52b9994884e52dd2a70178363acd0ce4845715
GitHub <noreply@github.com>
2020-03-02 06:27:49 +0000

    7d9fd19f4 fix: payment details page - force focus cancel button (#6511)

--- shared/components/card/card.module.d.ts
+++ shared/components/card/card.module.d.ts
@@ -3,9 +3,10 @@ import * as ɵngcc1 from './card.component';
 import * as ɵngcc2 from '@angular/common';
 import * as ɵngcc3 from '@spartacus/core';
 import * as ɵngcc4 from '../../../cms-components/misc/icon/icon.module';
+import * as ɵngcc5 from '../../directives/auto-focus/auto-focus.directive.module';
 export declare class CardModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CardModule, [typeof ɵngcc1.CardComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.I18nModule, typeof ɵngcc4.IconModule], [typeof ɵngcc1.CardComponent]>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CardModule, [typeof ɵngcc1.CardComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.I18nModule, typeof ɵngcc4.IconModule, typeof ɵngcc5.AutoFocusDirectiveModule], [typeof ɵngcc1.CardComponent]>;
     static ɵinj: ɵngcc0.ɵɵInjectorDef<CardModule>;
 }
 
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2FyZC5tb2R1bGUuZC50cyIsInNvdXJjZXMiOlsiY2FyZC5tb2R1bGUuZC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBOzs7QUFDQTsiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVjbGFyZSBjbGFzcyBDYXJkTW9kdWxlIHtcbn1cbiJdfQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2FyZC5tb2R1bGUuZC50cyIsInNvdXJjZXMiOlsiY2FyZC5tb2R1bGUuZC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQTs7O0FBQ0E7Iiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlY2xhcmUgY2xhc3MgQ2FyZE1vZHVsZSB7XG59XG4iXX0=
