41c50b9810634d2ed99596376d7bf8489a3e7213
GitHub <noreply@github.com>
2020-03-12 14:15:35 +0000

    3daea3b39 feat: Expose ConfigValidatorModule in public API (#6893)

--- recipes/storefront-foundation.module.d.ts
+++ recipes/storefront-foundation.module.d.ts
@@ -4,7 +4,7 @@ import * as ɵngcc2 from '../cms-structure/routing/routing.module';
 import * as ɵngcc3 from '../shared/config/view-config.module';
 import * as ɵngcc4 from '../layout/layout.module';
 export declare class StorefrontFoundationModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<StorefrontFoundationModule, never, [typeof ɵngcc1.StateModule, typeof ɵngcc1.AuthModule, typeof ɵngcc1.AnonymousConsentsModule, typeof ɵngcc1.ConfigModule, typeof ɵngcc1.ConfigInitializerModule, typeof ɵngcc2.RoutingModule, typeof ɵngcc1.I18nModule, typeof ɵngcc1.CmsModule, typeof ɵngcc1.GlobalMessageModule, typeof ɵngcc1.ProcessModule, typeof ɵngcc1.CartModule, typeof ɵngcc1.CheckoutModule, typeof ɵngcc1.UserModule, typeof ɵngcc1.ProductModule, typeof ɵngcc3.ViewConfigModule, typeof ɵngcc1.FeaturesConfigModule, typeof ɵngcc4.LayoutModule], [typeof ɵngcc4.LayoutModule]>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<StorefrontFoundationModule, never, [typeof ɵngcc1.StateModule, typeof ɵngcc1.AuthModule, typeof ɵngcc1.AnonymousConsentsModule, typeof ɵngcc1.ConfigModule, typeof ɵngcc1.ConfigInitializerModule, typeof ɵngcc1.ConfigValidatorModule, typeof ɵngcc2.RoutingModule, typeof ɵngcc1.I18nModule, typeof ɵngcc1.CmsModule, typeof ɵngcc1.GlobalMessageModule, typeof ɵngcc1.ProcessModule, typeof ɵngcc1.CartModule, typeof ɵngcc1.CheckoutModule, typeof ɵngcc1.UserModule, typeof ɵngcc1.ProductModule, typeof ɵngcc3.ViewConfigModule, typeof ɵngcc1.FeaturesConfigModule, typeof ɵngcc4.LayoutModule], [typeof ɵngcc4.LayoutModule]>;
     static ɵinj: ɵngcc0.ɵɵInjectorDef<StorefrontFoundationModule>;
 }
 
