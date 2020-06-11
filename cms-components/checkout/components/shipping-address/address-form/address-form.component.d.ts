3ff914cd5573b3adf70dd4c4147bf3bcc5491c4d
GitHub <noreply@github.com>
2020-06-11 20:14:42 +0000

    93be11db1 Default address checkbox should not be displayed on checkout if there are no addresses (GH-7617) (#7786)

--- cms-components/checkout/components/shipping-address/address-form/address-form.component.d.ts
+++ cms-components/checkout/components/shipping-address/address-form/address-form.component.d.ts
@@ -14,6 +14,7 @@ export declare class AddressFormComponent implements OnInit, OnDestroy {
     titles$: Observable<Title[]>;
     regions$: Observable<Region[]>;
     selectedCountry$: BehaviorSubject<string>;
+    addresses$: Observable<Address[]>;
     addressData: Address;
     actionBtnLabel: string;
     cancelBtnLabel: string;
