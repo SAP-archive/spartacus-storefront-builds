6a17d0d49c64d5f9345212789e5c0ecc3d69446f
GitHub <noreply@github.com>
2020-07-20 11:44:50 +0000

    d7095c013 fix: possibility to close tabs when they are already open (#8265)

--- cms-components/content/tab-paragraph-container/tab-paragraph-container.component.d.ts
+++ cms-components/content/tab-paragraph-container/tab-paragraph-container.component.d.ts
@@ -3,17 +3,20 @@ import { CmsService, CMSTabParagraphContainer, WindowRef } from '@spartacus/core
 import { Observable, Subscription } from 'rxjs';
 import { ComponentWrapperDirective } from '../../../cms-structure/page/component/component-wrapper.directive';
 import { CmsComponentData } from '../../../cms-structure/page/model/index';
+import { BreakpointService } from '../../../layout/breakpoint/breakpoint.service';
 export declare class TabParagraphContainerComponent implements AfterViewInit, OnInit, OnDestroy {
     componentData: CmsComponentData<CMSTabParagraphContainer>;
     protected cmsService: CmsService;
     protected winRef: WindowRef;
+    protected breakpointService: BreakpointService;
     activeTabNum: number;
     children: QueryList<ComponentWrapperDirective>;
     tabTitleParams: Observable<any>[];
     subscription: Subscription;
-    constructor(componentData: CmsComponentData<CMSTabParagraphContainer>, cmsService: CmsService, winRef: WindowRef);
+    tabSubscription: Subscription;
+    constructor(componentData: CmsComponentData<CMSTabParagraphContainer>, cmsService: CmsService, winRef: WindowRef, breakpointService: BreakpointService);
     components$: Observable<any[]>;
-    select(tabNum: number): void;
+    select(tabNum: number, event: any): void;
     ngOnInit(): void;
     ngAfterViewInit(): void;
     private getTitleParams;
--- fesm2015/spartacus-storefront.js
+++ fesm2015/spartacus-storefront.js
@@ -11419,10 +11419,11 @@ CmsParagraphModule = __decorate([
 ], CmsParagraphModule);
 
 let TabParagraphContainerComponent = class TabParagraphContainerComponent {
-    constructor(componentData, cmsService, winRef) {
+    constructor(componentData, cmsService, winRef, breakpointService) {
         this.componentData = componentData;
         this.cmsService = cmsService;
         this.winRef = winRef;
+        this.breakpointService = breakpointService;
         this.activeTabNum = 0;
         this.tabTitleParams = [];
         this.components$ = this.componentData.data$.pipe(distinctUntilChanged((x, y) => (x === null || x === void 0 ? void 0 : x.components) === (y === null || y === void 0 ? void 0 : y.components)), switchMap((data) => {
@@ -11438,8 +11439,18 @@ let TabParagraphContainerComponent = class TabParagraphContainerComponent {
             }))));
         }));
     }
-    select(tabNum) {
-        this.activeTabNum = tabNum;
+    select(tabNum, event) {
+        this.tabSubscription = this.breakpointService
+            .isDown(BREAKPOINT.sm)
+            .subscribe((res) => {
+            if (res) {
+                this.activeTabNum = this.activeTabNum === tabNum ? -1 : tabNum;
+                window.scrollTo(0, event.path[1].offsetTop);
+            }
+            else {
+                this.activeTabNum = tabNum;
+            }
+        });
     }
     ngOnInit() {
         var _a, _b, _c, _d;
@@ -11471,12 +11482,16 @@ let TabParagraphContainerComponent = class TabParagraphContainerComponent {
         if (this.subscription) {
             this.subscription.unsubscribe();
         }
+        if (this.tabSubscription) {
+            this.tabSubscription.unsubscribe();
+        }
     }
 };
 TabParagraphContainerComponent.ctorParameters = () => [
     { type: CmsComponentData },
     { type: CmsService },
-    { type: WindowRef }
+    { type: WindowRef },
+    { type: BreakpointService }
 ];
 __decorate([
     ViewChildren(ComponentWrapperDirective)
@@ -11484,7 +11499,7 @@ __decorate([
 TabParagraphContainerComponent = __decorate([
     Component({
         selector: 'cx-tab-paragraph-container',
-        template: "<ng-container *ngFor=\"let component of components$ | async; let i = index\">\n  <ng-container *ngIf=\"component\">\n    <button [class.active]=\"i === activeTabNum\" (click)=\"select(i)\">\n      {{ component.title | cxTranslate: { param: tabTitleParams[i] | async } }}\n    </button>\n    <div [class.active]=\"i === activeTabNum\">\n      <ng-template [cxOutlet]=\"component.flexType\" [cxOutletContext]=\"{}\">\n        <ng-container [cxComponentWrapper]=\"component\"></ng-container>\n      </ng-template>\n    </div>\n  </ng-container>\n</ng-container>\n",
+        template: "<ng-container *ngFor=\"let component of components$ | async; let i = index\">\n  <ng-container *ngIf=\"component\">\n    <button [class.active]=\"i === activeTabNum\" (click)=\"select(i, $event)\">\n      {{ component.title | cxTranslate: { param: tabTitleParams[i] | async } }}\n    </button>\n    <div [class.active]=\"i === activeTabNum\">\n      <ng-template [cxOutlet]=\"component.flexType\" [cxOutletContext]=\"{}\">\n        <ng-container [cxComponentWrapper]=\"component\"></ng-container>\n      </ng-template>\n    </div>\n  </ng-container>\n</ng-container>\n",
         changeDetection: ChangeDetectionStrategy.OnPush
     })
 ], TabParagraphContainerComponent);
