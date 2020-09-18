2d6200eae029d6f91514cab22b5df441d3e6cb8a
GitHub <noreply@github.com>
2020-09-18 10:43:03 +0000

    73c2859ab Release/3.0.0 next.1 (#8950)

--- cms-components/cart/cart-shared/order-summary/order-summary.component.d.ts
+++ cms-components/cart/cart-shared/order-summary/order-summary.component.d.ts
@@ -1,9 +1,4 @@
 import { Cart } from '@spartacus/core';
-import * as ɵngcc0 from '@angular/core';
 export declare class OrderSummaryComponent {
     cart: Cart;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<OrderSummaryComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<OrderSummaryComponent, "cx-order-summary", never, { "cart": "cart"; }, {}, never, never>;
 }
-
-//# sourceMappingURL=order-summary.component.d.ts.map
--- cms-components/checkout/components/checkout-progress/checkout-progress-mobile-bottom/checkout-progress-mobile-bottom.component.d.ts
+++ cms-components/checkout/components/checkout-progress/checkout-progress-mobile-bottom/checkout-progress-mobile-bottom.component.d.ts
@@ -2,7 +2,6 @@ import { ChangeDetectorRef, OnDestroy, OnInit } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { CheckoutStep } from '../../../model/checkout-step.model';
 import { CheckoutStepService } from '../../../services/checkout-step.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class CheckoutProgressMobileBottomComponent implements OnInit, OnDestroy {
     protected checkoutStepService: CheckoutStepService;
     protected cdr: ChangeDetectorRef;
@@ -13,8 +12,4 @@ export declare class CheckoutProgressMobileBottomComponent implements OnInit, On
     subscription: Subscription;
     ngOnInit(): void;
     ngOnDestroy(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CheckoutProgressMobileBottomComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<CheckoutProgressMobileBottomComponent, "cx-checkout-progress-mobile-bottom", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=checkout-progress-mobile-bottom.component.d.ts.map
