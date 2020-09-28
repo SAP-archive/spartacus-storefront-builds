724a7e2269f4ce9b2d140f9f4ea8a0c61fd16de0
GitHub <noreply@github.com>
2020-09-28 14:48:19 +0000

    cd0c1e48c feat: My-company improvements (#9070)

--- cms-components/product/stock-notification/stock-notification.component.d.ts
+++ cms-components/product/stock-notification/stock-notification.component.d.ts
@@ -3,7 +3,6 @@ import { UserInterestsService, UserNotificationPreferenceService, AuthService, G
 import { Observable } from 'rxjs';
 import { CurrentProductService } from '../current-product.service';
 import { ModalService } from '../../../shared/components/modal/modal.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class StockNotificationComponent implements OnInit, OnDestroy {
     private authService;
     private currentProductService;
@@ -29,8 +28,4 @@ export declare class StockNotificationComponent implements OnInit, OnDestroy {
     private onInterestAddingError;
     private openDialog;
     ngOnDestroy(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<StockNotificationComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<StockNotificationComponent, "cx-stock-notification", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=stock-notification.component.d.ts.map
