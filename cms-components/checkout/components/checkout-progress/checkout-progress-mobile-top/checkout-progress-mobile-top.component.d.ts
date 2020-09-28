a39600ac8fb7182d329000042668129230098128
GitHub <noreply@github.com>
2020-09-28 19:48:36 +0000

    af51d1883 fix b2b checkout after merge (#9080)

--- cms-components/checkout/components/checkout-progress/checkout-progress-mobile-top/checkout-progress-mobile-top.component.d.ts
+++ cms-components/checkout/components/checkout-progress/checkout-progress-mobile-top/checkout-progress-mobile-top.component.d.ts
@@ -3,6 +3,7 @@ import { ActiveCartService, Cart } from '@spartacus/core';
 import { Observable, Subscription } from 'rxjs';
 import { CheckoutStep } from '../../../model/checkout-step.model';
 import { CheckoutStepService } from '../../../services/checkout-step.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class CheckoutProgressMobileTopComponent implements OnInit, OnDestroy {
     protected checkoutStepService: CheckoutStepService;
     protected activeCartService: ActiveCartService;
@@ -15,4 +16,8 @@ export declare class CheckoutProgressMobileTopComponent implements OnInit, OnDes
     subscription: Subscription;
     ngOnInit(): void;
     ngOnDestroy(): void;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<CheckoutProgressMobileTopComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<CheckoutProgressMobileTopComponent, "cx-checkout-progress-mobile-top", never, {}, {}, never, never>;
 }
+
+//# sourceMappingURL=checkout-progress-mobile-top.component.d.ts.map
--- cms-components/storefinder/components/store-finder-grid/store-finder-grid.component.d.ts
+++ cms-components/storefinder/components/store-finder-grid/store-finder-grid.component.d.ts
@@ -2,6 +2,7 @@ import { OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StoreFinderService, GeoPoint } from '@spartacus/core';
 import { Observable } from 'rxjs';
+import * as ɵngcc0 from '@angular/core';
 export declare class StoreFinderGridComponent implements OnInit, OnDestroy {
     private storeFinderService;
     private route;
@@ -13,4 +14,8 @@ export declare class StoreFinderGridComponent implements OnInit, OnDestroy {
     constructor(storeFinderService: StoreFinderService, route: ActivatedRoute);
     ngOnInit(): void;
     ngOnDestroy(): void;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<StoreFinderGridComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<StoreFinderGridComponent, "cx-store-finder-grid", never, {}, {}, never, never>;
 }
+
+//# sourceMappingURL=store-finder-grid.component.d.ts.map
