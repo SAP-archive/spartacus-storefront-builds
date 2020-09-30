f293e51e43536d026f969090f9039f4786d7370c
GitHub <noreply@github.com>
2020-09-30 12:29:28 +0000

    0abaa69c0 feat: add unit test for HttpParamsURIEncoder (#9089)

--- cms-structure/outlet/outlet.module.d.ts
+++ cms-structure/outlet/outlet.module.d.ts
@@ -1,9 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './outlet.directive';
-import * as ɵngcc2 from '@angular/common';
 export declare class OutletModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<OutletModule, [typeof ɵngcc1.OutletDirective], [typeof ɵngcc2.CommonModule], [typeof ɵngcc1.OutletDirective]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<OutletModule>;
 }
-
-//# sourceMappingURL=outlet.module.d.ts.map
