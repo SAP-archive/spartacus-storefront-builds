641eaf9ea1c3f83ce64a665068f14f53a5496730
GitHub <noreply@github.com>
2020-09-30 19:00:26 +0000

    9b5cc4646 fix: Remove unnecessary cdref (#8998)

--- cms-components/myaccount/order/return-request-detail/return-request-items/return-request-items.component.d.ts.__ivy_ngcc_bak
+++
@@ -1,8 +0,0 @@
-import { Observable } from 'rxjs';
-import { ReturnRequest } from '@spartacus/core';
-import { ReturnRequestService } from '../return-request.service';
-export declare class ReturnRequestItemsComponent {
-    protected returnRequestService: ReturnRequestService;
-    constructor(returnRequestService: ReturnRequestService);
-    returnRequest$: Observable<ReturnRequest>;
-}
--- cms-structure/page/page-layout/page-layout.component.d.ts
+++ cms-structure/page/page-layout/page-layout.component.d.ts
@@ -1,7 +1,6 @@
 import { ElementRef, Renderer2 } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { PageLayoutService } from './page-layout.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class PageLayoutComponent {
     private el;
     private renderer;
@@ -15,8 +14,4 @@ export declare class PageLayoutComponent {
     private currentClass;
     constructor(el: ElementRef, renderer: Renderer2, pageLayoutService: PageLayoutService);
     set styleClass(cls: string);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<PageLayoutComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<PageLayoutComponent, "cx-page-layout", never, { "section": "section"; }, {}, never, ["*"]>;
 }
-
-//# sourceMappingURL=page-layout.component.d.ts.map
