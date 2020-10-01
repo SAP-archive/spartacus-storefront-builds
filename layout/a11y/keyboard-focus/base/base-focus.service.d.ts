8ae6e386606197fed8f02b7d4b4a9abbed3e4bb4
GitHub <noreply@github.com>
2020-10-01 20:26:12 +0000

    29569e9c6 feat: Epic/b2b scheduled repl (GH-8202)

--- layout/a11y/keyboard-focus/base/base-focus.service.d.ts
+++ layout/a11y/keyboard-focus/base/base-focus.service.d.ts
@@ -1,6 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
 export declare abstract class BaseFocusService {
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<BaseFocusService, never>;
 }
-
-//# sourceMappingURL=base-focus.service.d.ts.map
