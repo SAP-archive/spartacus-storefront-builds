f20da73f5cda74bb2e2aefbeff3d78f5c082c877
GitHub <noreply@github.com>
2020-10-02 19:22:47 +0000

    4b697c972 chore: Update dep with security warning in api extractor (#9130)

--- cms-components/asm/asm-session-timer/format-timer.pipe.d.ts.__ivy_ngcc_bak
+++
@@ -1,4 +0,0 @@
-import { PipeTransform } from '@angular/core';
-export declare class FormatTimerPipe implements PipeTransform {
-    transform(totalSeconds: number): string;
-}
--- cms-components/checkout/components/checkout-order-summary/checkout-order-summary.component.d.ts
+++ cms-components/checkout/components/checkout-order-summary/checkout-order-summary.component.d.ts
@@ -1,12 +1,7 @@
 import { ActiveCartService, Cart } from '@spartacus/core';
 import { Observable } from 'rxjs';
-import * as ɵngcc0 from '@angular/core';
 export declare class CheckoutOrderSummaryComponent {
     protected activeCartService: ActiveCartService;
     cart$: Observable<Cart>;
     constructor(activeCartService: ActiveCartService);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CheckoutOrderSummaryComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<CheckoutOrderSummaryComponent, "cx-checkout-order-summary", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=checkout-order-summary.component.d.ts.map
