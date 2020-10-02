1b84c192d05c763b30f1cc9b90b83bb1a42cff8c
GitHub <noreply@github.com>
2020-10-02 10:26:46 +0000

    0d70b1928 Allow BASE_URL and API_PREFIX to override env config (#9110)

--- cms-components/checkout/components/delivery-mode/delivery-mode.module.d.ts
+++ cms-components/checkout/components/delivery-mode/delivery-mode.module.d.ts
@@ -1,12 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './delivery-mode.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@angular/forms';
-import * as ɵngcc4 from '@spartacus/core';
-import * as ɵngcc5 from '../../../../shared/components/spinner/spinner.module';
 export declare class DeliveryModeModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<DeliveryModeModule, [typeof ɵngcc1.DeliveryModeComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.ReactiveFormsModule, typeof ɵngcc4.I18nModule, typeof ɵngcc5.SpinnerModule], [typeof ɵngcc1.DeliveryModeComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<DeliveryModeModule>;
 }
-
-//# sourceMappingURL=delivery-mode.module.d.ts.map
