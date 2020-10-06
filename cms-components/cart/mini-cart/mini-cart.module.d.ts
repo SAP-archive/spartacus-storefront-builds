8f21256905a196025ed871ba48cd915c5bcb8646
GitHub <noreply@github.com>
2020-10-06 14:15:21 +0000

    b807da746 GH-8750 No need to clone repo when installing libs from npm (#8751)

--- cms-components/cart/mini-cart/mini-cart.module.d.ts
+++ cms-components/cart/mini-cart/mini-cart.module.d.ts
@@ -1,12 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './mini-cart.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@angular/router';
-import * as ɵngcc4 from '@spartacus/core';
-import * as ɵngcc5 from '../../misc/icon/icon.module';
 export declare class MiniCartModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<MiniCartModule, [typeof ɵngcc1.MiniCartComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.RouterModule, typeof ɵngcc4.UrlModule, typeof ɵngcc5.IconModule, typeof ɵngcc4.I18nModule], [typeof ɵngcc1.MiniCartComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<MiniCartModule>;
 }
-
-//# sourceMappingURL=mini-cart.module.d.ts.map
