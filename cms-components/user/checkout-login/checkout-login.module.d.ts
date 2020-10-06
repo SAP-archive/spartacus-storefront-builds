884bd161c80686cb45557da68b6a19984fc033f5
GitHub <noreply@github.com>
2020-10-06 22:03:29 +0000

    693ef1890 small directionality fixes on my company (#9220)

--- cms-components/user/checkout-login/checkout-login.module.d.ts
+++ cms-components/user/checkout-login/checkout-login.module.d.ts
@@ -1,12 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './checkout-login.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@spartacus/core';
-import * as ɵngcc4 from '@angular/forms';
-import * as ɵngcc5 from '../../../shared/components/form/form-errors/form-errors.module';
 export declare class CheckoutLoginModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CheckoutLoginModule, [typeof ɵngcc1.CheckoutLoginComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.I18nModule, typeof ɵngcc4.FormsModule, typeof ɵngcc4.ReactiveFormsModule, typeof ɵngcc4.FormsModule, typeof ɵngcc4.ReactiveFormsModule, typeof ɵngcc5.FormErrorsModule], [typeof ɵngcc1.CheckoutLoginComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<CheckoutLoginModule>;
 }
-
-//# sourceMappingURL=checkout-login.module.d.ts.map
