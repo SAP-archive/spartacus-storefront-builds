6d1bde826515d71e818881aee1eecba549b677bb
GitHub <noreply@github.com>
2020-10-06 04:51:23 +0000

    d64807462 feat: B2B checkout replenishment order E2E support (GH-9139)

--- cms-components/storefinder/store-finder.module.d.ts
+++ cms-components/storefinder/store-finder.module.d.ts
@@ -1,28 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './components/store-finder-search/store-finder-search.component';
-import * as ɵngcc2 from './components/store-finder-search-result/store-finder-list/store-finder-list.component';
-import * as ɵngcc3 from './components/store-finder-map/store-finder-map.component';
-import * as ɵngcc4 from './components/store-finder-list-item/store-finder-list-item.component';
-import * as ɵngcc5 from './components/store-finder-stores-count/store-finder-stores-count.component';
-import * as ɵngcc6 from './components/store-finder-grid/store-finder-grid.component';
-import * as ɵngcc7 from './components/store-finder-store-description/store-finder-store-description.component';
-import * as ɵngcc8 from './components/schedule-component/schedule.component';
-import * as ɵngcc9 from './components/store-finder-header/store-finder-header.component';
-import * as ɵngcc10 from './components/store-finder-search-result/store-finder-search-result.component';
-import * as ɵngcc11 from './components/store-finder/store-finder.component';
-import * as ɵngcc12 from './components/store-finder-pagination-details/store-finder-pagination-details.component';
-import * as ɵngcc13 from './components/store-finder-store/store-finder-store.component';
-import * as ɵngcc14 from '@angular/common';
-import * as ɵngcc15 from '@angular/forms';
-import * as ɵngcc16 from '@angular/router';
-import * as ɵngcc17 from '../../shared/components/list-navigation/list-navigation.module';
-import * as ɵngcc18 from '@ng-bootstrap/ng-bootstrap';
-import * as ɵngcc19 from '../../shared/components/spinner/spinner.module';
-import * as ɵngcc20 from '@spartacus/core';
-import * as ɵngcc21 from '../misc/icon/icon.module';
 export declare class StoreFinderModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<StoreFinderModule, [typeof ɵngcc1.StoreFinderSearchComponent, typeof ɵngcc2.StoreFinderListComponent, typeof ɵngcc3.StoreFinderMapComponent, typeof ɵngcc4.StoreFinderListItemComponent, typeof ɵngcc5.StoreFinderStoresCountComponent, typeof ɵngcc6.StoreFinderGridComponent, typeof ɵngcc7.StoreFinderStoreDescriptionComponent, typeof ɵngcc8.ScheduleComponent, typeof ɵngcc9.StoreFinderHeaderComponent, typeof ɵngcc10.StoreFinderSearchResultComponent, typeof ɵngcc11.StoreFinderComponent, typeof ɵngcc12.StoreFinderPaginationDetailsComponent, typeof ɵngcc13.StoreFinderStoreComponent], [typeof ɵngcc14.CommonModule, typeof ɵngcc15.ReactiveFormsModule, typeof ɵngcc16.RouterModule, typeof ɵngcc17.ListNavigationModule, typeof ɵngcc18.NgbTabsetModule, typeof ɵngcc19.SpinnerModule, typeof ɵngcc20.UrlModule, typeof ɵngcc20.StoreFinderCoreModule, typeof ɵngcc20.I18nModule, typeof ɵngcc21.IconModule], [typeof ɵngcc8.ScheduleComponent, typeof ɵngcc11.StoreFinderComponent, typeof ɵngcc6.StoreFinderGridComponent, typeof ɵngcc9.StoreFinderHeaderComponent, typeof ɵngcc4.StoreFinderListItemComponent, typeof ɵngcc3.StoreFinderMapComponent, typeof ɵngcc12.StoreFinderPaginationDetailsComponent, typeof ɵngcc1.StoreFinderSearchComponent, typeof ɵngcc10.StoreFinderSearchResultComponent, typeof ɵngcc2.StoreFinderListComponent, typeof ɵngcc7.StoreFinderStoreDescriptionComponent, typeof ɵngcc5.StoreFinderStoresCountComponent, typeof ɵngcc13.StoreFinderStoreComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<StoreFinderModule>;
 }
-
-//# sourceMappingURL=store-finder.module.d.ts.map
