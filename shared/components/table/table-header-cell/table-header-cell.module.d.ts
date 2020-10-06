c72f7a064a0ae84a92aee3c9bb72afbfa6074d05
GitHub <noreply@github.com>
2020-10-06 12:41:47 +0000

    5665b5e21 fix: locales must be driven by the domain entity rather than the table configuration type (#9209)

--- shared/components/table/table-header-cell/table-header-cell.module.d.ts
+++ shared/components/table/table-header-cell/table-header-cell.module.d.ts
@@ -1,10 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './table-header-cell.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@spartacus/core';
 export declare class TableHeaderCellModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<TableHeaderCellModule, [typeof ɵngcc1.TableHeaderCellComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.I18nModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<TableHeaderCellModule>;
 }
-
-//# sourceMappingURL=table-header-cell.module.d.ts.map
