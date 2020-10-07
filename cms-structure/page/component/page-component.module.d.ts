bfa154ae1fc8010d12c3f3804c10123827d4bccc
GitHub <noreply@github.com>
2020-10-07 21:22:41 +0000

    98bcbca0c [GH-8679] Upgrading cypress and enabling test retries (#9158)

--- cms-structure/page/component/page-component.module.d.ts
+++ cms-structure/page/component/page-component.module.d.ts
@@ -1,9 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './component-wrapper.directive';
-import * as ɵngcc2 from '@angular/common';
 export declare class PageComponentModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<PageComponentModule, [typeof ɵngcc1.ComponentWrapperDirective], [typeof ɵngcc2.CommonModule], [typeof ɵngcc1.ComponentWrapperDirective]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<PageComponentModule>;
 }
-
-//# sourceMappingURL=page-component.module.d.ts.map
