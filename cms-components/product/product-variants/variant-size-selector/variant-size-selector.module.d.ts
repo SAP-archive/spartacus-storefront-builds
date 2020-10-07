78c8a7d3b52d4c6e2c7e0dd73acd293ed1b55e42
GitHub <noreply@github.com>
2020-10-07 13:27:35 +0000

    d3f05465f Major release checklist (#9096)

--- cms-components/product/product-variants/variant-size-selector/variant-size-selector.module.d.ts
+++ cms-components/product/product-variants/variant-size-selector/variant-size-selector.module.d.ts
@@ -1,11 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './variant-size-selector.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@angular/router';
-import * as ɵngcc4 from '@spartacus/core';
 export declare class VariantSizeSelectorModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<VariantSizeSelectorModule, [typeof ɵngcc1.VariantSizeSelectorComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.RouterModule, typeof ɵngcc4.UrlModule, typeof ɵngcc4.I18nModule], [typeof ɵngcc1.VariantSizeSelectorComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<VariantSizeSelectorModule>;
 }
-
-//# sourceMappingURL=variant-size-selector.module.d.ts.map
