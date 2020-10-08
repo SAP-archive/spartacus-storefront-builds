d9c519a298dac822dbe243a1698dad30c688cfa6
GitHub <noreply@github.com>
2020-10-08 16:51:19 +0000

    9552c9f08 fix: reference items by entry number (GH-9134)

--- layout/a11y/keyboard-focus/lock/lock-focus.service.d.ts
+++ layout/a11y/keyboard-focus/lock/lock-focus.service.d.ts
@@ -1,7 +1,3 @@
 import { TrapFocusService } from '../trap/trap-focus.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class LockFocusService extends TrapFocusService {
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<LockFocusService, never>;
 }
-
-//# sourceMappingURL=lock-focus.service.d.ts.map
