ad26a7942a15dddcd7cb0d3e2047f5ecf32cd280
GitHub <noreply@github.com>
2020-10-09 13:58:04 +0000

    578069770 feat: send spartacus events to cds-profiletag event-layer (GH-5687)

--- shared/components/order-overview/order-overview.module.d.ts
+++ shared/components/order-overview/order-overview.module.d.ts
@@ -1,11 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './order-overview.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@spartacus/core';
-import * as ɵngcc4 from '../card/card.module';
 export declare class OrderOverviewModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<OrderOverviewModule, [typeof ɵngcc1.OrderOverviewComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.I18nModule, typeof ɵngcc4.CardModule], [typeof ɵngcc1.OrderOverviewComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<OrderOverviewModule>;
 }
-
-//# sourceMappingURL=order-overview.module.d.ts.map
