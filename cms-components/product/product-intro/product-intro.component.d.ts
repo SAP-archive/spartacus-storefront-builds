5a80c999ba95d5e57980f1ca6a4f621b82373a4e
GitHub <noreply@github.com>
2020-10-09 19:11:11 +0000

    400240d89 chore: add default when process env args are not provided for cds, b2b, cdc (#9290)

--- cms-components/product/product-intro/product-intro.component.d.ts
+++ cms-components/product/product-intro/product-intro.component.d.ts
@@ -2,7 +2,6 @@ import { AfterContentChecked } from '@angular/core';
 import { Product, TranslationService, WindowRef } from '@spartacus/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { CurrentProductService } from '../current-product.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class ProductIntroComponent implements AfterContentChecked {
     protected currentProductService: CurrentProductService;
     private translationService;
@@ -16,8 +15,4 @@ export declare class ProductIntroComponent implements AfterContentChecked {
     private getTabsComponent;
     private clickTabIfInactive;
     private getTabByLabel;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<ProductIntroComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<ProductIntroComponent, "cx-product-intro", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=product-intro.component.d.ts.map
