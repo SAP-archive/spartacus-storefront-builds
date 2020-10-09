5c2f0821a6ac7beaecb22dcdca4d2ff263098f8d
GitHub <noreply@github.com>
2020-10-09 17:11:50 +0000

    d156713e1 test: A11y tests for b2b (#9124)

--- recipes/b2c-storefront.module.d.ts
+++ recipes/b2c-storefront.module.d.ts
@@ -1,5 +1,12 @@
 import { ModuleWithProviders } from '@angular/core';
 import { StorefrontConfig } from '../storefront-config';
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './storefront.module';
+import * as ɵngcc2 from '../cms-components/cms-lib.module';
 export declare class B2cStorefrontModule {
     static withConfig(config?: StorefrontConfig): ModuleWithProviders<B2cStorefrontModule>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<B2cStorefrontModule, never, [typeof ɵngcc1.StorefrontModule, typeof ɵngcc2.CmsLibModule], [typeof ɵngcc1.StorefrontModule]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<B2cStorefrontModule>;
 }
+
+//# sourceMappingURL=b2c-storefront.module.d.ts.map
