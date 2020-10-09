9aaa3ec06eae641fb679533db7bf8d7f5cb60880
GitHub <noreply@github.com>
2020-10-09 06:06:06 +0000

    2846ef579 feat: Create child unit (#9229)

--- cms-components/product/product-variants/variant-color-selector/variant-color-selector.module.d.ts
+++ cms-components/product/product-variants/variant-color-selector/variant-color-selector.module.d.ts
@@ -1,2 +1,11 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './variant-color-selector.component';
+import * as ɵngcc2 from '@angular/common';
+import * as ɵngcc3 from '@angular/router';
+import * as ɵngcc4 from '@spartacus/core';
 export declare class VariantColorSelectorModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<VariantColorSelectorModule, [typeof ɵngcc1.VariantColorSelectorComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.RouterModule, typeof ɵngcc4.UrlModule, typeof ɵngcc4.I18nModule], [typeof ɵngcc1.VariantColorSelectorComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<VariantColorSelectorModule>;
 }
+
+//# sourceMappingURL=variant-color-selector.module.d.ts.map
