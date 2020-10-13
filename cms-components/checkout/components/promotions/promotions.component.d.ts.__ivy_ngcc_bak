c3885f2f0d53ab9b0118cfe990b8a7d6f26482ef
GitHub <noreply@github.com>
2020-10-13 09:28:57 +0000

    45c2f8386 Updated ccv2 base url (#9296)

--- cms-components/checkout/components/promotions/promotions.component.d.ts.__ivy_ngcc_bak
+++
@@ -1,5 +0,0 @@
-import { Promotion } from '@spartacus/core';
-export declare class PromotionsComponent {
-    promotions: Promotion[];
-    constructor();
-}
--- layout/a11y/skip-link/component/skip-link.component.d.ts
+++ layout/a11y/skip-link/component/skip-link.component.d.ts
@@ -1,14 +1,9 @@
 import { Observable } from 'rxjs';
 import { SkipLink } from '../config/skip-link.config';
 import { SkipLinkService } from '../service/skip-link.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class SkipLinkComponent {
     private skipLinkService;
     skipLinks$: Observable<SkipLink[]>;
     constructor(skipLinkService: SkipLinkService);
     scrollToTarget(skipLink: SkipLink): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<SkipLinkComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<SkipLinkComponent, "cx-skip-link", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=skip-link.component.d.ts.map
