6b2ada18b2c2744d746b99b7c4af35995ce77bd1
GitHub <noreply@github.com>
2020-10-13 22:16:44 +0000

    2dff81ad4 Update Slack invite link (#9311)

--- cms-components/product/product-variants/variant-style-icons/variant-style-icons.module.d.ts
+++ cms-components/product/product-variants/variant-style-icons/variant-style-icons.module.d.ts
@@ -1,11 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './variant-style-icons.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@angular/router';
-import * as ɵngcc4 from '@spartacus/core';
 export declare class VariantStyleIconsModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<VariantStyleIconsModule, [typeof ɵngcc1.VariantStyleIconsComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.RouterModule, typeof ɵngcc4.UrlModule, typeof ɵngcc4.I18nModule], [typeof ɵngcc1.VariantStyleIconsComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<VariantStyleIconsModule>;
 }
-
-//# sourceMappingURL=variant-style-icons.module.d.ts.map
