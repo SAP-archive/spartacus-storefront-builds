75b0c344e8e30a48e266eb232130c3bb655f0564
GitHub <noreply@github.com>
2020-10-13 16:45:41 +0000

    6ec89e1a5 Fix broken node-sass build (#9305)

--- cms-structure/routing/routing.module.d.ts
+++ cms-structure/routing/routing.module.d.ts
@@ -1,11 +1,4 @@
 import { ModuleWithProviders } from '@angular/core';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@spartacus/core';
-import * as ɵngcc2 from './cms-route/cms-route.module';
 export declare class RoutingModule {
     static forRoot(): ModuleWithProviders<RoutingModule>;
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<RoutingModule, never, [typeof ɵngcc1.RoutingModule, typeof ɵngcc2.CmsRouteModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<RoutingModule>;
 }
-
-//# sourceMappingURL=routing.module.d.ts.map
