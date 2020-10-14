aab2e04816811211323d86930e30206b2552675e
GitHub <noreply@github.com>
2020-10-14 04:51:40 +0000

    291989f66 feat: Make converter service aware of lazy-loaded features (#9303)

--- cms-components/checkout/components/payment-method/payment-form/payment-form.module.d.ts
+++ cms-components/checkout/components/payment-method/payment-form/payment-form.module.d.ts
@@ -1,2 +1,16 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './payment-form.component';
+import * as ɵngcc2 from '@angular/common';
+import * as ɵngcc3 from '@angular/forms';
+import * as ɵngcc4 from '@ng-select/ng-select';
+import * as ɵngcc5 from '../../../../../shared/components/card/card.module';
+import * as ɵngcc6 from '@spartacus/core';
+import * as ɵngcc7 from '../../../../misc/icon/icon.module';
+import * as ɵngcc8 from '../../../../../shared/components/spinner/spinner.module';
+import * as ɵngcc9 from '../../../../../shared/components/form/form-errors/form-errors.module';
 export declare class PaymentFormModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<PaymentFormModule, [typeof ɵngcc1.PaymentFormComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.ReactiveFormsModule, typeof ɵngcc4.NgSelectModule, typeof ɵngcc5.CardModule, typeof ɵngcc6.I18nModule, typeof ɵngcc7.IconModule, typeof ɵngcc8.SpinnerModule, typeof ɵngcc9.FormErrorsModule], [typeof ɵngcc1.PaymentFormComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<PaymentFormModule>;
 }
+
+//# sourceMappingURL=payment-form.module.d.ts.map
