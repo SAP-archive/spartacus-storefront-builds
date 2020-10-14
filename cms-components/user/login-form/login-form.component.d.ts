4c77821a6fbea2836a71f1115a095661ea4813af
GitHub <noreply@github.com>
2020-10-14 08:30:44 +0000

    26c77ffa6 Added new libs for releasing (#9300)

--- cms-components/user/login-form/login-form.component.d.ts
+++ cms-components/user/login-form/login-form.component.d.ts
@@ -2,6 +2,7 @@ import { OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthRedirectService, AuthService, GlobalMessageService, WindowRef } from '@spartacus/core';
 import { Subscription } from 'rxjs';
+import * as ɵngcc0 from '@angular/core';
 export declare class LoginFormComponent implements OnInit, OnDestroy {
     protected auth: AuthService;
     protected globalMessageService: GlobalMessageService;
@@ -15,4 +16,8 @@ export declare class LoginFormComponent implements OnInit, OnDestroy {
     submitForm(): void;
     ngOnDestroy(): void;
     protected loginUser(): void;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<LoginFormComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<LoginFormComponent, "cx-login-form", never, {}, {}, never, never>;
 }
+
+//# sourceMappingURL=login-form.component.d.ts.map
--- events/cart/cart-page-event.module.d.ts
+++ events/cart/cart-page-event.module.d.ts
@@ -1,4 +1,9 @@
 import { CartPageEventBuilder } from './cart-page-event.builder';
+import * as ɵngcc0 from '@angular/core';
 export declare class CartPageEventModule {
     constructor(_cartPageEventBuilder: CartPageEventBuilder);
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CartPageEventModule, never, never, never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<CartPageEventModule>;
 }
+
+//# sourceMappingURL=cart-page-event.module.d.ts.map
