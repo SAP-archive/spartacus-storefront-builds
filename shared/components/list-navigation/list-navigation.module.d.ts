f484ee96d9387f8368ed75d4030f97ee343ad45d
GitHub <noreply@github.com>
2020-10-14 11:54:20 +0000

    7e1f9c53b feat: Enable lazy loading for my-account/organization feature (#9277)

--- shared/components/list-navigation/list-navigation.module.d.ts
+++ shared/components/list-navigation/list-navigation.module.d.ts
@@ -1,2 +1,14 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './sorting/sorting.component';
+import * as ɵngcc2 from '@angular/common';
+import * as ɵngcc3 from '@ng-select/ng-select';
+import * as ɵngcc4 from '@angular/forms';
+import * as ɵngcc5 from '../../../cms-components/misc/icon/icon.module';
+import * as ɵngcc6 from './pagination/pagination.module';
+import * as ɵngcc7 from './pagination/pagination.component';
 export declare class ListNavigationModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ListNavigationModule, [typeof ɵngcc1.SortingComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.NgSelectModule, typeof ɵngcc4.FormsModule, typeof ɵngcc4.ReactiveFormsModule, typeof ɵngcc5.IconModule, typeof ɵngcc6.PaginationModule], [typeof ɵngcc1.SortingComponent, typeof ɵngcc7.PaginationComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<ListNavigationModule>;
 }
+
+//# sourceMappingURL=list-navigation.module.d.ts.map
