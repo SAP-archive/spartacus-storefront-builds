b05624af3534f85844c9f4ca893a9f800c7a51e2
GitHub <noreply@github.com>
2020-10-15 11:39:13 +0000

    2955be184 refactor: unit tree component (#8894)

--- cms-components/product/product-list/product-facet-navigation/active-facets/active-facets.module.d.ts
+++ cms-components/product/product-list/product-facet-navigation/active-facets/active-facets.module.d.ts
@@ -1,2 +1,13 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './active-facets.component';
+import * as ɵngcc2 from '@angular/common';
+import * as ɵngcc3 from '@angular/router';
+import * as ɵngcc4 from '@spartacus/core';
+import * as ɵngcc5 from '../../../../misc/icon/icon.module';
+import * as ɵngcc6 from '../../../../../layout/a11y/keyboard-focus/keyboard-focus.module';
 export declare class ActiveFacetsModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ActiveFacetsModule, [typeof ɵngcc1.ActiveFacetsComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.RouterModule, typeof ɵngcc4.I18nModule, typeof ɵngcc5.IconModule, typeof ɵngcc6.KeyboardFocusModule], [typeof ɵngcc1.ActiveFacetsComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<ActiveFacetsModule>;
 }
+
+//# sourceMappingURL=active-facets.module.d.ts.map
