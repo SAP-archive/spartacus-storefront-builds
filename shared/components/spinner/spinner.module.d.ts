605244a381bc56f379ab02326bb116f6a9a0e6ef
GitHub <noreply@github.com>
2020-10-16 14:43:34 +0000

    17186522c feat: renamed my-account library to organization; renamed organization feature to administration (#9376)

--- shared/components/spinner/spinner.module.d.ts
+++ shared/components/spinner/spinner.module.d.ts
@@ -1,10 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './spinner.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@spartacus/core';
 export declare class SpinnerModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<SpinnerModule, [typeof ɵngcc1.SpinnerComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.I18nModule], [typeof ɵngcc1.SpinnerComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<SpinnerModule>;
 }
-
-//# sourceMappingURL=spinner.module.d.ts.map
