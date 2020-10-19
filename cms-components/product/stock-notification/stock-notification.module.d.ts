e13e17786b4d067b8db2bbbd6f8e222199b549ca
GitHub <noreply@github.com>
2020-10-19 15:15:52 +0000

    5fb02cb76 fix: bring back units address active state styling (#9394)

--- cms-components/product/stock-notification/stock-notification.module.d.ts
+++ cms-components/product/stock-notification/stock-notification.module.d.ts
@@ -1,13 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './stock-notification.component';
-import * as ɵngcc2 from './stock-notification-dialog/stock-notification-dialog.component';
-import * as ɵngcc3 from '@angular/common';
-import * as ɵngcc4 from '@angular/router';
-import * as ɵngcc5 from '@spartacus/core';
-import * as ɵngcc6 from '../../../shared/components/spinner/spinner.module';
 export declare class StockNotificationModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<StockNotificationModule, [typeof ɵngcc1.StockNotificationComponent, typeof ɵngcc2.StockNotificationDialogComponent], [typeof ɵngcc3.CommonModule, typeof ɵngcc4.RouterModule, typeof ɵngcc5.I18nModule, typeof ɵngcc6.SpinnerModule, typeof ɵngcc5.UrlModule], [typeof ɵngcc1.StockNotificationComponent, typeof ɵngcc2.StockNotificationDialogComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<StockNotificationModule>;
 }
-
-//# sourceMappingURL=stock-notification.module.d.ts.map
