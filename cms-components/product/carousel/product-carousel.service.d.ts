543b18c5f9d5100842289f3675e45b33f7193625
GitHub <noreply@github.com>
2020-10-19 19:34:49 +0000

    c992bad4e fix: Disable permission type select in Purchase limit edit form #9354 (#9405)

--- cms-components/product/carousel/product-carousel.service.d.ts
+++ cms-components/product/carousel/product-carousel.service.d.ts
@@ -1,6 +1,7 @@
 import { ProductReferenceService, ProductService, SemanticPathService } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { ProductCarouselItem } from './product-carousel.model';
+import * as ɵngcc0 from '@angular/core';
 export declare class ProductCarouselService {
     protected productService: ProductService;
     protected referenceService: ProductReferenceService;
@@ -15,4 +16,7 @@ export declare class ProductCarouselService {
      * Converts the product to a generic CarouselItem
      */
     private convertProduct;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<ProductCarouselService, never>;
 }
+
+//# sourceMappingURL=product-carousel.service.d.ts.map
