1adbc7dd8ff0b091bc6d7240eff2666971d39022
GitHub <noreply@github.com>
2020-10-19 18:30:28 +0000

    736066b3c fix: Prevent API requests to be called with anonymous user

--- cms-components/product/product-list/product-facet-navigation/facet/facet.module.d.ts
+++ cms-components/product/product-list/product-facet-navigation/facet/facet.module.d.ts
@@ -1,13 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './facet.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@angular/router';
-import * as ɵngcc4 from '@spartacus/core';
-import * as ɵngcc5 from '../../../../misc/icon/icon.module';
-import * as ɵngcc6 from '../../../../../layout/a11y/keyboard-focus/keyboard-focus.module';
 export declare class FacetModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<FacetModule, [typeof ɵngcc1.FacetComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.RouterModule, typeof ɵngcc4.UrlModule, typeof ɵngcc4.I18nModule, typeof ɵngcc5.IconModule, typeof ɵngcc6.KeyboardFocusModule], [typeof ɵngcc1.FacetComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<FacetModule>;
 }
-
-//# sourceMappingURL=facet.module.d.ts.map
--- cms-structure/page/page-layout/page-template.directive.d.ts
+++ cms-structure/page/page-layout/page-template.directive.d.ts
@@ -25,7 +25,6 @@ import { PageLayoutService } from './page-layout.service';
  * ```
  *
  */
-import * as ɵngcc0 from '@angular/core';
 export declare class PageTemplateDirective implements OnInit, OnDestroy {
     protected pageLayoutService: PageLayoutService;
     protected elementRef: ElementRef;
@@ -61,8 +60,4 @@ export declare class PageTemplateDirective implements OnInit, OnDestroy {
      */
     protected get host(): HTMLElement;
     ngOnDestroy(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<PageTemplateDirective, [null, null, { optional: true; }]>;
-    static ɵdir: ɵngcc0.ɵɵDirectiveDefWithMeta<PageTemplateDirective, "[cxPageTemplateStyle]", never, { "cxPageTemplateStyle": "cxPageTemplateStyle"; }, {}, never>;
 }
-
-//# sourceMappingURL=page-template.directive.d.ts.map
