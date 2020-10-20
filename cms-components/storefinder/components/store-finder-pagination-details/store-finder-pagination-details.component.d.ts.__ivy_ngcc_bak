dbf650ad9c448de449445b9e2da156948af9e545
GitHub <noreply@github.com>
2020-10-20 20:50:12 +0000

    1e860ba85 Fix: cart-item-list handles null item list properly. (#8881)

--- cms-components/storefinder/components/store-finder-pagination-details/store-finder-pagination-details.component.d.ts.__ivy_ngcc_bak
+++
@@ -1,6 +0,0 @@
-import { PaginationModel } from '@spartacus/core';
-export declare class StoreFinderPaginationDetailsComponent {
-    pagination: PaginationModel;
-    constructor();
-    getResultsPerPage(): string;
-}
--- shared/components/table/table-data-cell/table-data-cell.component.d.ts
+++ shared/components/table/table-data-cell/table-data-cell.component.d.ts
@@ -1,6 +1,5 @@
 import { OutletContextData } from '../../../../cms-structure/outlet/outlet.model';
 import { TableHeaderOutletContext } from '../table.model';
-import * as ɵngcc0 from '@angular/core';
 export declare class TableDataCellComponent {
     protected outlet: OutletContextData<TableHeaderOutletContext>;
     cls: boolean;
@@ -8,8 +7,4 @@ export declare class TableDataCellComponent {
     get value(): string;
     protected get model(): any;
     protected get field(): string;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<TableDataCellComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<TableDataCellComponent, "ng-component", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=table-data-cell.component.d.ts.map
