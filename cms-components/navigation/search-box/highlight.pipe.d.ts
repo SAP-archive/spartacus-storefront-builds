00b8c14600eb29e0cc7de11ccfcbda93aa4bff1b
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2020-10-20 19:34:32 +0000

    35e0ce929 Revert "feat: My Company E2E Skilet and Some Tests (#7845)"

--- cms-components/navigation/search-box/highlight.pipe.d.ts
+++ cms-components/navigation/search-box/highlight.pipe.d.ts
@@ -1,4 +1,9 @@
 import { PipeTransform } from '@angular/core';
+import * as ɵngcc0 from '@angular/core';
 export declare class HighlightPipe implements PipeTransform {
     transform(text: string, match?: string): string;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<HighlightPipe, never>;
+    static ɵpipe: ɵngcc0.ɵɵPipeDefWithMeta<HighlightPipe, "cxHighlight">;
 }
+
+//# sourceMappingURL=highlight.pipe.d.ts.map
---
+++ events/product/product-page-event.module.d.ts.__ivy_ngcc_bak
@@ -0,0 +1,4 @@
+import { ProductPageEventBuilder } from './product-page-event.builder';
+export declare class ProductPageEventModule {
+    constructor(_productPageEventBuilder: ProductPageEventBuilder);
+}
