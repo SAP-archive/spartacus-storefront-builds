18ed5ba85a3100a4d4973e48e98c765f5479a281
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2020-10-20 19:46:35 +0000

    35e0ce929 Revert "feat: My Company E2E Skilet and Some Tests (#7845)"

--- cms-components/checkout/guards/checkout-auth.guard.d.ts
+++ cms-components/checkout/guards/checkout-auth.guard.d.ts
@@ -2,7 +2,6 @@ import { CanActivate } from '@angular/router';
 import { ActiveCartService, AuthRedirectService, AuthService, RoutingService, UserService, GlobalMessageService } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { CheckoutConfigService } from '../services/checkout-config.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class CheckoutAuthGuard implements CanActivate {
     protected routingService: RoutingService;
     protected authService: AuthService;
@@ -13,7 +12,4 @@ export declare class CheckoutAuthGuard implements CanActivate {
     protected globalMessageService: GlobalMessageService;
     constructor(routingService: RoutingService, authService: AuthService, authRedirectService: AuthRedirectService, checkoutConfigService: CheckoutConfigService, activeCartService: ActiveCartService, userService: UserService, globalMessageService: GlobalMessageService);
     canActivate(): Observable<boolean>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CheckoutAuthGuard, never>;
 }
-
-//# sourceMappingURL=checkout-auth.guard.d.ts.map
