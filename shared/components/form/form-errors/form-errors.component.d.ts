17beee8ef092f35dfdb6842b2601904296f24f7b
GitHub <noreply@github.com>
2020-10-20 15:24:22 +0000

    5e43e0a89 fix: adjusting max-width limit for search box (#9412)

--- shared/components/form/form-errors/form-errors.component.d.ts
+++ shared/components/form/form-errors/form-errors.component.d.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs';
 /**
  * This component renders form errors.
  */
-import * as ɵngcc0 from '@angular/core';
 export declare class FormErrorsComponent {
     _control: FormControl;
     errors$: Observable<string[]>;
@@ -15,8 +14,4 @@ export declare class FormErrorsComponent {
     get invalid(): boolean;
     get dirty(): boolean;
     get touched(): boolean;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<FormErrorsComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<FormErrorsComponent, "cx-form-errors", never, { "control": "control"; "translationParams": "translationParams"; }, {}, never, never>;
 }
-
-//# sourceMappingURL=form-errors.component.d.ts.map
