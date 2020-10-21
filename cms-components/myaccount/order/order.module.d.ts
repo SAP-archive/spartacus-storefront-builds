4298fd429bcbbf2ca02e1c6528d68e4d8019e4c2
GitHub <noreply@github.com>
2020-10-21 07:52:55 +0000

    1da3d163b chore: move cds to integration libs (#9426)

--- cms-components/myaccount/order/order.module.d.ts
+++ cms-components/myaccount/order/order.module.d.ts
@@ -1,2 +1,15 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './order-history/order-history.module';
+import * as ɵngcc2 from './order-details/order-details.module';
+import * as ɵngcc3 from './replenishment-order-details/replenishment-order-details.module';
+import * as ɵngcc4 from './amend-order/cancellations/order-cancellation.module';
+import * as ɵngcc5 from './amend-order/returns/order-return.module';
+import * as ɵngcc6 from './replenishment-order-history/replenishment-order-history.module';
+import * as ɵngcc7 from './return-request-list/order-return-request-list.module';
+import * as ɵngcc8 from './return-request-detail/return-request-detail.module';
 export declare class OrderModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<OrderModule, never, [typeof ɵngcc1.OrderHistoryModule, typeof ɵngcc2.OrderDetailsModule, typeof ɵngcc3.ReplenishmentOrderDetailsModule, typeof ɵngcc4.OrderCancellationModule, typeof ɵngcc5.OrderReturnModule, typeof ɵngcc6.ReplenishmentOrderHistoryModule, typeof ɵngcc7.ReturnRequestListModule, typeof ɵngcc8.ReturnRequestDetailModule], never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<OrderModule>;
 }
+
+//# sourceMappingURL=order.module.d.ts.map
