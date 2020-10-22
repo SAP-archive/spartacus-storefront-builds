38c4819ae522ccbf36009b43b9fe836c12852b29
GitHub <noreply@github.com>
2020-10-22 01:06:16 +0000

    cb6409aa8 fix: Details page load action firing when navigating outside of the page (GH-9367)

--- cms-components/cart/cart-page-layout-handler.d.ts
+++ cms-components/cart/cart-page-layout-handler.d.ts
@@ -1,13 +1,9 @@
 import { ActiveCartService, SelectiveCartService } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { PageLayoutHandler } from '../../cms-structure/page/page-layout/page-layout-handler';
-import * as ɵngcc0 from '@angular/core';
 export declare class CartPageLayoutHandler implements PageLayoutHandler {
     protected activeCartService: ActiveCartService;
     protected selectiveCartService: SelectiveCartService;
     constructor(activeCartService: ActiveCartService, selectiveCartService: SelectiveCartService);
     handle(slots$: Observable<string[]>, pageTemplate?: string, section?: string): Observable<any>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CartPageLayoutHandler, never>;
 }
-
-//# sourceMappingURL=cart-page-layout-handler.d.ts.map
--- cms-components/checkout/components/checkout-orchestrator/checkout-orchestrator.component.d.ts
+++ cms-components/checkout/components/checkout-orchestrator/checkout-orchestrator.component.d.ts
@@ -1,8 +1,3 @@
-import * as ɵngcc0 from '@angular/core';
 export declare class CheckoutOrchestratorComponent {
     constructor();
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CheckoutOrchestratorComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<CheckoutOrchestratorComponent, "cx-checkout-orchestrator", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=checkout-orchestrator.component.d.ts.map
