71555ebac71e095ccba472a2e03acedce2f3710c
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2020-10-24 06:39:37 +0000

    efaadda6f Adding netlify reference

--- layout/a11y/keyboard-focus/focus.directive.d.ts
+++ layout/a11y/keyboard-focus/focus.directive.d.ts
@@ -2,7 +2,6 @@ import { ElementRef, Renderer2 } from '@angular/core';
 import { FocusConfig } from './keyboard-focus.model';
 import { LockFocusDirective } from './lock/lock-focus.directive';
 import { KeyboardFocusService } from './services/keyboard-focus.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class FocusDirective extends LockFocusDirective {
     protected elementRef: ElementRef;
     protected service: KeyboardFocusService;
@@ -10,8 +9,4 @@ export declare class FocusDirective extends LockFocusDirective {
     protected defaultConfig: FocusConfig;
     protected config: FocusConfig;
     constructor(elementRef: ElementRef, service: KeyboardFocusService, renderer: Renderer2);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<FocusDirective, never>;
-    static ɵdir: ɵngcc0.ɵɵDirectiveDefWithMeta<FocusDirective, "[cxFocus]", never, { "config": "cxFocus"; }, {}, never>;
 }
-
-//# sourceMappingURL=focus.directive.d.ts.map
--- layout/header/hamburger-menu/hamburger-menu.module.d.ts
+++ layout/header/hamburger-menu/hamburger-menu.module.d.ts
@@ -1,9 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './hamburger-menu.component';
-import * as ɵngcc2 from '@angular/common';
 export declare class HamburgerMenuModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<HamburgerMenuModule, [typeof ɵngcc1.HamburgerMenuComponent], [typeof ɵngcc2.CommonModule], [typeof ɵngcc1.HamburgerMenuComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<HamburgerMenuModule>;
 }
-
-//# sourceMappingURL=hamburger-menu.module.d.ts.map
