1418c7aaf3a90665ebd62003d4efa761a3c4765b
GitHub <noreply@github.com>
2020-10-25 18:09:43 +0000

    5b0c5da8e fix expand / collapse all + further simplfiy and optimize (#9507)

--- cms-components/storefinder/components/store-finder-grid/store-finder-grid.component.d.ts
+++ cms-components/storefinder/components/store-finder-grid/store-finder-grid.component.d.ts
@@ -2,7 +2,6 @@ import { OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StoreFinderService, GeoPoint } from '@spartacus/core';
 import { Observable } from 'rxjs';
-import * as ɵngcc0 from '@angular/core';
 export declare class StoreFinderGridComponent implements OnInit, OnDestroy {
     private storeFinderService;
     private route;
@@ -14,8 +13,4 @@ export declare class StoreFinderGridComponent implements OnInit, OnDestroy {
     constructor(storeFinderService: StoreFinderService, route: ActivatedRoute);
     ngOnInit(): void;
     ngOnDestroy(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<StoreFinderGridComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<StoreFinderGridComponent, "cx-store-finder-grid", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=store-finder-grid.component.d.ts.map
