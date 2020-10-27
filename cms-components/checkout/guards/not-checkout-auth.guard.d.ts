e28420b955621380d5da0e7ae26239cdca6c82be
GitHub <noreply@github.com>
2020-10-27 09:37:05 +0000

    00382dac2 fix: looped unit get request on address details (#9515)

--- cms-components/checkout/guards/not-checkout-auth.guard.d.ts
+++ cms-components/checkout/guards/not-checkout-auth.guard.d.ts
@@ -1,14 +1,10 @@
 import { CanActivate } from '@angular/router';
 import { ActiveCartService, AuthService, RoutingService } from '@spartacus/core';
 import { Observable } from 'rxjs';
-import * as ɵngcc0 from '@angular/core';
 export declare class NotCheckoutAuthGuard implements CanActivate {
     protected routingService: RoutingService;
     protected authService: AuthService;
     protected activeCartService: ActiveCartService;
     constructor(routingService: RoutingService, authService: AuthService, activeCartService: ActiveCartService);
     canActivate(): Observable<boolean>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<NotCheckoutAuthGuard, never>;
 }
-
-//# sourceMappingURL=not-checkout-auth.guard.d.ts.map
