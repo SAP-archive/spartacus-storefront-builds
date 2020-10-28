f66175041d2c0d49e99c530a2a8c4cfd84cbd988
GitHub <noreply@github.com>
2020-10-28 18:06:49 +0000

    89e41f19a feat: provide observable context$ inside OutletContextData (#9431)

--- cms-components/myaccount/order/return-request-detail/return-request-items/return-request-items.component.d.ts
+++ cms-components/myaccount/order/return-request-detail/return-request-items/return-request-items.component.d.ts
@@ -1,8 +1,13 @@
 import { Observable } from 'rxjs';
 import { ReturnRequest } from '@spartacus/core';
 import { ReturnRequestService } from '../return-request.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class ReturnRequestItemsComponent {
     protected returnRequestService: ReturnRequestService;
     constructor(returnRequestService: ReturnRequestService);
     returnRequest$: Observable<ReturnRequest>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<ReturnRequestItemsComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<ReturnRequestItemsComponent, "cx-return-request-items", never, {}, {}, never, never>;
 }
+
+//# sourceMappingURL=return-request-items.component.d.ts.map
