d949655112c97f985dd0658a772f3a3ccd4a4578
GitHub <noreply@github.com>
2020-10-29 12:50:43 +0000

    c2e33a0c1 feat: unwrapping themes-for-minors due to major release (#9439)

--- cms-components/myaccount/update-email/update-email-form/update-email-form.component.d.ts
+++ cms-components/myaccount/update-email/update-email-form/update-email-form.component.d.ts
@@ -1,6 +1,5 @@
 import { EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import * as ɵngcc0 from '@angular/core';
 export declare class UpdateEmailFormComponent {
     private fb;
     saveEmail: EventEmitter<{
@@ -12,8 +11,4 @@ export declare class UpdateEmailFormComponent {
     constructor(fb: FormBuilder);
     onSubmit(): void;
     onCancel(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<UpdateEmailFormComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<UpdateEmailFormComponent, "cx-update-email-form", never, {}, { "saveEmail": "saveEmail"; "cancelEmail": "cancelEmail"; }, never, never>;
 }
-
-//# sourceMappingURL=update-email-form.component.d.ts.map
