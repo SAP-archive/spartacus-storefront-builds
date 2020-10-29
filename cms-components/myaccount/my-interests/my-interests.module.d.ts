d71c6db5cd1c982f0352d26b9c15d07612fed47d
GitHub <noreply@github.com>
2020-10-29 18:10:17 +0000

    aa949f713 Fix release-it config for orgranization (#9582)

--- cms-components/myaccount/my-interests/my-interests.module.d.ts
+++ cms-components/myaccount/my-interests/my-interests.module.d.ts
@@ -1,14 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './my-interests.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@spartacus/core';
-import * as ɵngcc4 from '@angular/router';
-import * as ɵngcc5 from '../../../shared/components/list-navigation/list-navigation.module';
-import * as ɵngcc6 from '../../../shared/components/media/media.module';
-import * as ɵngcc7 from '../../../shared/components/spinner/spinner.module';
 export declare class MyInterestsModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<MyInterestsModule, [typeof ɵngcc1.MyInterestsComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.I18nModule, typeof ɵngcc4.RouterModule, typeof ɵngcc5.ListNavigationModule, typeof ɵngcc3.I18nModule, typeof ɵngcc3.UrlModule, typeof ɵngcc6.MediaModule, typeof ɵngcc7.SpinnerModule], [typeof ɵngcc1.MyInterestsComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<MyInterestsModule>;
 }
-
-//# sourceMappingURL=my-interests.module.d.ts.map
