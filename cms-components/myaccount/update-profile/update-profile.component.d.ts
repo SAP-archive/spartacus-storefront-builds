f14d00350b868014b02211e18f29259d51c5a6ca
GitHub <noreply@github.com>
2020-10-29 20:42:54 +0000

    deedd101d feat: Disable B2B user Spartacus fix #9456 (#9565)

--- cms-components/myaccount/update-profile/update-profile.component.d.ts
+++ cms-components/myaccount/update-profile/update-profile.component.d.ts
@@ -1,7 +1,6 @@
 import { OnDestroy, OnInit } from '@angular/core';
 import { GlobalMessageService, RoutingService, Title, User, UserService } from '@spartacus/core';
 import { Observable } from 'rxjs';
-import * as ɵngcc0 from '@angular/core';
 export declare class UpdateProfileComponent implements OnInit, OnDestroy {
     private routingService;
     private userService;
@@ -18,8 +17,4 @@ export declare class UpdateProfileComponent implements OnInit, OnDestroy {
         userUpdates: User;
     }): void;
     ngOnDestroy(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<UpdateProfileComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<UpdateProfileComponent, "cx-update-profile", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=update-profile.component.d.ts.map
