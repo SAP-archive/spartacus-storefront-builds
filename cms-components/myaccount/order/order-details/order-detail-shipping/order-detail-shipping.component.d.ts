a7e135609233309b3b2ac86b0677c6b167aee60b
GitHub <noreply@github.com>
2020-10-29 20:33:38 +0000

    deedd101d feat: Disable B2B user Spartacus fix #9456 (#9565)

--- cms-components/myaccount/order/order-details/order-detail-shipping/order-detail-shipping.component.d.ts
+++ cms-components/myaccount/order/order-details/order-detail-shipping/order-detail-shipping.component.d.ts
@@ -1,9 +1,14 @@
 import { OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { OrderDetailsService } from '../order-details.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class OrderDetailShippingComponent implements OnInit {
     protected orderDetailsService: OrderDetailsService;
     constructor(orderDetailsService: OrderDetailsService);
     order$: Observable<any>;
     ngOnInit(): void;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<OrderDetailShippingComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<OrderDetailShippingComponent, "cx-order-details-shipping", never, {}, {}, never, never>;
 }
+
+//# sourceMappingURL=order-detail-shipping.component.d.ts.map
--- cms-components/product/product-list/product-facet-navigation/facet-list/facet-list.module.d.ts
+++ cms-components/product/product-list/product-facet-navigation/facet-list/facet-list.module.d.ts
@@ -1,2 +1,13 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './facet-list.component';
+import * as ɵngcc2 from '@angular/common';
+import * as ɵngcc3 from '@spartacus/core';
+import * as ɵngcc4 from '../../../../misc/icon/icon.module';
+import * as ɵngcc5 from '../facet/facet.module';
+import * as ɵngcc6 from '../../../../../layout/a11y/keyboard-focus/keyboard-focus.module';
 export declare class FacetListModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<FacetListModule, [typeof ɵngcc1.FacetListComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.I18nModule, typeof ɵngcc4.IconModule, typeof ɵngcc5.FacetModule, typeof ɵngcc6.KeyboardFocusModule], [typeof ɵngcc1.FacetListComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<FacetListModule>;
 }
+
+//# sourceMappingURL=facet-list.module.d.ts.map
