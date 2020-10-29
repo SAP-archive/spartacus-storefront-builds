0dbf990dbdb8fe97332dd36807beb71b937c021b
GitHub <noreply@github.com>
2020-10-29 19:01:19 +0000

    388694e37 test: Order Approvals e2e tests (#8220)

--- cms-components/cart/cart-details/cart-details.component.d.ts
+++ cms-components/cart/cart-details/cart-details.component.d.ts
@@ -3,6 +3,7 @@ import { ActiveCartService, AuthService, Cart, OrderEntry, PromotionLocation, Pr
 import { Observable } from 'rxjs';
 import { PromotionService } from '../../../shared/services/promotion/promotion.service';
 import { Item } from '../cart-shared/cart-item/cart-item.component';
+import * as ɵngcc0 from '@angular/core';
 export declare class CartDetailsComponent implements OnInit {
     protected activeCartService: ActiveCartService;
     protected promotionService: PromotionService;
@@ -20,4 +21,8 @@ export declare class CartDetailsComponent implements OnInit {
     constructor(activeCartService: ActiveCartService, promotionService: PromotionService, selectiveCartService: SelectiveCartService, authService: AuthService, routingService: RoutingService);
     ngOnInit(): void;
     saveForLater(item: Item): void;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<CartDetailsComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<CartDetailsComponent, "cx-cart-details", never, {}, {}, never, never>;
 }
+
+//# sourceMappingURL=cart-details.component.d.ts.map
