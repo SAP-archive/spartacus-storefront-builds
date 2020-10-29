9a9836a5be691dd76d6d6b9dbafaf6473b0d6fc0
GitHub <noreply@github.com>
2020-10-29 12:39:26 +0000

    c2e33a0c1 feat: unwrapping themes-for-minors due to major release (#9439)

---
+++ cms-components/content/link/link.component.d.ts.__ivy_ngcc_bak
@@ -0,0 +1,6 @@
+import { CmsLinkComponent } from '@spartacus/core';
+import { CmsComponentData } from '../../../cms-structure/page/model/cms-component-data';
+export declare class LinkComponent {
+    component: CmsComponentData<CmsLinkComponent>;
+    constructor(component: CmsComponentData<CmsLinkComponent>);
+}
--- cms-components/user/logout/logout.guard.d.ts
+++ cms-components/user/logout/logout.guard.d.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs';
  * the homepage. If the homepage is protected, the user is redirected
  * to the login route instead.
  */
+import * as ɵngcc0 from '@angular/core';
 export declare class LogoutGuard implements CanActivate {
     protected auth: AuthService;
     protected cms: CmsService;
@@ -25,4 +26,7 @@ export declare class LogoutGuard implements CanActivate {
      * (in case of a closed shop). We'll redirect to the login page instead.
      */
     protected getRedirectUrl(): UrlTree;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<LogoutGuard, never>;
 }
+
+//# sourceMappingURL=logout.guard.d.ts.map
