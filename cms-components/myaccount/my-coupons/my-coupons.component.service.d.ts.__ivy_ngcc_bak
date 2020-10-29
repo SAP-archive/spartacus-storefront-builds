0ef8f790a4e99f971e47e6fbb41a3c68abf43b64
GitHub <noreply@github.com>
2020-10-29 18:01:59 +0000

    aa949f713 Fix release-it config for orgranization (#9582)

---
+++ cms-components/myaccount/my-coupons/my-coupons.component.service.d.ts.__ivy_ngcc_bak
@@ -0,0 +1,23 @@
+import { CustomerCoupon, RoutingService, TranslationService } from '@spartacus/core';
+import { Observable } from 'rxjs';
+export declare class MyCouponsComponentService {
+    protected routingService: RoutingService;
+    protected translation: TranslationService;
+    sortLabels: Observable<{
+        byStartDateAsc: string;
+        byStartDateDesc: string;
+        byEndDateAsc: string;
+        byEndDateDesc: string;
+    }>;
+    protected readonly RELEVANCE = ":relevance";
+    protected readonly CUSTOMER_COUPON_CODE = ":customerCouponCode:";
+    constructor(routingService: RoutingService, translation: TranslationService);
+    launchSearchPage(coupon: CustomerCoupon): void;
+    private buildSearchParam;
+    getSortLabels(): Observable<{
+        byStartDateAsc: string;
+        byStartDateDesc: string;
+        byEndDateAsc: string;
+        byEndDateDesc: string;
+    }>;
+}
--- cms-components/storefinder/components/store-finder-search-result/store-finder-search-result.component.d.ts
+++ cms-components/storefinder/components/store-finder-search-result/store-finder-search-result.component.d.ts
@@ -2,6 +2,7 @@ import { OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GeoPoint, SearchConfig, StoreFinderSearchQuery, StoreFinderService, StoreFinderConfig } from '@spartacus/core';
 import { Observable, Subscription } from 'rxjs';
+import * as ɵngcc0 from '@angular/core';
 export declare class StoreFinderSearchResultComponent implements OnInit, OnDestroy {
     private storeFinderService;
     private route;
@@ -22,4 +23,8 @@ export declare class StoreFinderSearchResultComponent implements OnInit, OnDestr
     viewPage(pageNumber: number): void;
     private initialize;
     private parseParameters;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<StoreFinderSearchResultComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<StoreFinderSearchResultComponent, "cx-store-finder-search-result", never, {}, {}, never, never>;
 }
+
+//# sourceMappingURL=store-finder-search-result.component.d.ts.map
