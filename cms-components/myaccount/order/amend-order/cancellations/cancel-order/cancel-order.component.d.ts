d0ae4b385e1828b294a18a8584c221241aa7069b
GitHub <noreply@github.com>
2020-10-29 15:20:54 +0000

    52cef5743 feat: update organization schematics (#9496)

--- cms-components/myaccount/order/amend-order/cancellations/cancel-order/cancel-order.component.d.ts
+++ cms-components/myaccount/order/amend-order/cancellations/cancel-order/cancel-order.component.d.ts
@@ -2,15 +2,10 @@ import { FormGroup } from '@angular/forms';
 import { OrderEntry } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { OrderAmendService } from '../../amend-order.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class CancelOrderComponent {
     protected orderAmendService: OrderAmendService;
     orderCode: string;
     form$: Observable<FormGroup>;
     entries$: Observable<OrderEntry[]>;
     constructor(orderAmendService: OrderAmendService);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CancelOrderComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<CancelOrderComponent, "cx-cancel-order", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=cancel-order.component.d.ts.map
