7696f30e7ede7312412f3febb79d58f898624a51
GitHub <noreply@github.com>
2020-10-30 23:31:11 +0000

    a53b24c34 fix: ProductReferenceComponent: reference products are not updated and displayed correctly (GH-8223)

--- cms-components/checkout/components/payment-method/payment-method.module.d.ts
+++ cms-components/checkout/components/payment-method/payment-method.module.d.ts
@@ -1,14 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './payment-method.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@angular/router';
-import * as ɵngcc4 from './payment-form/payment-form.module';
-import * as ɵngcc5 from '../../../../shared/components/card/card.module';
-import * as ɵngcc6 from '../../../../shared/components/spinner/spinner.module';
-import * as ɵngcc7 from '@spartacus/core';
 export declare class PaymentMethodModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<PaymentMethodModule, [typeof ɵngcc1.PaymentMethodComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.RouterModule, typeof ɵngcc4.PaymentFormModule, typeof ɵngcc5.CardModule, typeof ɵngcc6.SpinnerModule, typeof ɵngcc7.I18nModule], [typeof ɵngcc1.PaymentMethodComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<PaymentMethodModule>;
 }
-
-//# sourceMappingURL=payment-method.module.d.ts.map
