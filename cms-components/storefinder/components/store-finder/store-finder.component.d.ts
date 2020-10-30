559d4f83f1a4a2c35c4b4f840b39365bf079ef7c
GitHub <noreply@github.com>
2020-10-30 15:10:39 +0000

    6885dded9 fix: Empty object request while create/update address #9572 (#9578)

--- cms-components/storefinder/components/store-finder/store-finder.component.d.ts
+++ cms-components/storefinder/components/store-finder/store-finder.component.d.ts
@@ -1,7 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
 export declare class StoreFinderComponent {
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<StoreFinderComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<StoreFinderComponent, "cx-store-finder", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=store-finder.component.d.ts.map
