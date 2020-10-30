3b48d97d1b7961529ce06ec606c8d260418d8665
GitHub <noreply@github.com>
2020-10-30 19:26:48 +0000

    dc6b55c94 chore: Anonymous consents - remove deprecated code (GH-8158)

--- cms-components/checkout/components/checkout-progress/checkout-progress-mobile-top/checkout-progress-mobile-top.module.d.ts
+++ cms-components/checkout/components/checkout-progress/checkout-progress-mobile-top/checkout-progress-mobile-top.module.d.ts
@@ -1,2 +1,11 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './checkout-progress-mobile-top.component';
+import * as ɵngcc2 from '@angular/common';
+import * as ɵngcc3 from '@spartacus/core';
+import * as ɵngcc4 from '@angular/router';
 export declare class CheckoutProgressMobileTopModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CheckoutProgressMobileTopModule, [typeof ɵngcc1.CheckoutProgressMobileTopComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.UrlModule, typeof ɵngcc3.I18nModule, typeof ɵngcc4.RouterModule], [typeof ɵngcc1.CheckoutProgressMobileTopComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<CheckoutProgressMobileTopModule>;
 }
+
+//# sourceMappingURL=checkout-progress-mobile-top.module.d.ts.map
