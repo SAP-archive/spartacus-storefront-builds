ba9beefc5510efe9d22070a34085a6eb6bb228b1
GitHub <noreply@github.com>
2020-11-02 14:50:49 +0000

    fa83076c8 refactor: Enable lazy loading for order approvals. (GH-9464)(#9594)

--- cms-components/product/product-intro/product-intro.module.d.ts
+++ cms-components/product/product-intro/product-intro.module.d.ts
@@ -1,2 +1,11 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './product-intro.component';
+import * as ɵngcc2 from '@angular/common';
+import * as ɵngcc3 from '@spartacus/core';
+import * as ɵngcc4 from '../../../shared/components/star-rating/star-rating.module';
 export declare class ProductIntroModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ProductIntroModule, [typeof ɵngcc1.ProductIntroComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.I18nModule, typeof ɵngcc4.StarRatingModule], [typeof ɵngcc1.ProductIntroComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<ProductIntroModule>;
 }
+
+//# sourceMappingURL=product-intro.module.d.ts.map
