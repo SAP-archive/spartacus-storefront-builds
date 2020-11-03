fed814d0b9b40327859808ded0ed7b612a8ee553
GitHub <noreply@github.com>
2020-11-03 11:42:22 +0000

    ba421a4c0 fix: Lazy loading causes SSR flickering (#9593)

--- cms-components/myaccount/close-account/components/close-account/close-account.component.d.ts
+++ cms-components/myaccount/close-account/components/close-account/close-account.component.d.ts
@@ -1,12 +1,7 @@
 import { ModalService } from '../../../../../shared/components/modal/index';
-import * as ɵngcc0 from '@angular/core';
 export declare class CloseAccountComponent {
     private modalService;
     modal: any;
     constructor(modalService: ModalService);
     openModal(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CloseAccountComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<CloseAccountComponent, "cx-close-account", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=close-account.component.d.ts.map
