c264e8f975f1d92af0bb1768ab3e254d14160a14
GitHub <noreply@github.com>
2020-11-03 20:15:21 +0000

    2a07eedfc Fix: Product attributes page now displays unit symbol with value if present. (#8845) (#9463)

--- cms-components/myaccount/address-book/address-book.component.service.d.ts
+++ cms-components/myaccount/address-book/address-book.component.service.d.ts
@@ -1,6 +1,5 @@
 import { Address, CheckoutDeliveryService, UserAddressService } from '@spartacus/core';
 import { Observable } from 'rxjs';
-import * as ɵngcc0 from '@angular/core';
 export declare class AddressBookComponentService {
     protected userAddressService: UserAddressService;
     protected checkoutDeliveryService: CheckoutDeliveryService;
@@ -10,7 +9,4 @@ export declare class AddressBookComponentService {
     loadAddresses(): void;
     addUserAddress(address: Address): void;
     updateUserAddress(addressId: string, address: Address): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<AddressBookComponentService, never>;
 }
-
-//# sourceMappingURL=address-book.component.service.d.ts.map
