7a97341cac3119606a18662f8b1b183f88c18011
GitHub <noreply@github.com>
2020-11-03 16:45:09 +0000

    7bda819fd fix: organization library should compile without core style library (#9637)

--- shared/components/table/table-data-cell/table-data-cell.module.d.ts
+++ shared/components/table/table-data-cell/table-data-cell.module.d.ts
@@ -1,9 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './table-data-cell.component';
-import * as ɵngcc2 from '@angular/common';
 export declare class TableDataCellModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<TableDataCellModule, [typeof ɵngcc1.TableDataCellComponent], [typeof ɵngcc2.CommonModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<TableDataCellModule>;
 }
-
-//# sourceMappingURL=table-data-cell.module.d.ts.map
