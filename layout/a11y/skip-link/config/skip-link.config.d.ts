26cd5b55576aac036140603446e558772aad11b1
GitHub <noreply@github.com>
2020-11-04 10:16:29 +0000

    006e3466d feat: schematics - enable feature selection in the organization library schematic (#9645)

--- layout/a11y/skip-link/config/skip-link.config.d.ts
+++ layout/a11y/skip-link/config/skip-link.config.d.ts
@@ -1,7 +1,5 @@
-import * as ɵngcc0 from '@angular/core';
 export declare abstract class SkipLinkConfig {
     skipLinks?: SkipLink[];
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<SkipLinkConfig, never>;
 }
 export declare abstract class SkipLink {
     key: string;
@@ -13,5 +11,3 @@ export declare enum SkipLinkScrollPosition {
     BEFORE = "BEFORE",
     AFTER = "AFTER"
 }
-
-//# sourceMappingURL=skip-link.config.d.ts.map
--- shared/components/list-navigation/sorting/sorting.component.d.ts
+++ shared/components/list-navigation/sorting/sorting.component.d.ts
@@ -1,6 +1,5 @@
 import { EventEmitter } from '@angular/core';
 import { SortModel } from '@spartacus/core';
-import * as ɵngcc0 from '@angular/core';
 export declare class SortingComponent {
     sortOptions: SortModel[];
     selectedOption: string;
@@ -11,8 +10,4 @@ export declare class SortingComponent {
     sortListEvent: EventEmitter<string>;
     constructor();
     sortList(sortCode: string): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<SortingComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<SortingComponent, "cx-sorting", never, { "sortOptions": "sortOptions"; "selectedOption": "selectedOption"; "placeholder": "placeholder"; "sortLabels": "sortLabels"; }, { "sortListEvent": "sortListEvent"; }, never, never>;
 }
-
-//# sourceMappingURL=sorting.component.d.ts.map
