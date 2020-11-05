ee8a419972857f012161235cef04bf064c8cb5e0
GitHub <noreply@github.com>
2020-11-05 08:05:55 +0000

    2e82eec14 fix: force AdminGuard to wait for user data

--- cms-components/checkout/components/checkout-orchestrator/checkout-orchestrator.module.d.ts
+++ cms-components/checkout/components/checkout-orchestrator/checkout-orchestrator.module.d.ts
@@ -1,2 +1,9 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './checkout-orchestrator.component';
+import * as ɵngcc2 from '@angular/common';
 export declare class CheckoutOrchestratorModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CheckoutOrchestratorModule, [typeof ɵngcc1.CheckoutOrchestratorComponent], [typeof ɵngcc2.CommonModule], [typeof ɵngcc1.CheckoutOrchestratorComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<CheckoutOrchestratorModule>;
 }
+
+//# sourceMappingURL=checkout-orchestrator.module.d.ts.map
