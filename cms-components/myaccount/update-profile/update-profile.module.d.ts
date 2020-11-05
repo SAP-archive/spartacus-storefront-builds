76f7ce9cd5237b6e31dcdede2c0aa2fcc906c733
GitHub <noreply@github.com>
2020-11-05 12:14:09 +0000

    e0de80ca6 chore: migration 3.0 - remove deprecated code and constructor from OutletService (#9657)

--- cms-components/myaccount/update-profile/update-profile.module.d.ts
+++ cms-components/myaccount/update-profile/update-profile.module.d.ts
@@ -1,14 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './update-profile.component';
-import * as ɵngcc2 from './components/update-profile-form.component';
-import * as ɵngcc3 from '@angular/common';
-import * as ɵngcc4 from '@angular/forms';
-import * as ɵngcc5 from '../../../shared/components/spinner/spinner.module';
-import * as ɵngcc6 from '@spartacus/core';
-import * as ɵngcc7 from '../../../shared/components/form/form-errors/form-errors.module';
 export declare class UpdateProfileModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<UpdateProfileModule, [typeof ɵngcc1.UpdateProfileComponent, typeof ɵngcc2.UpdateProfileFormComponent], [typeof ɵngcc3.CommonModule, typeof ɵngcc4.FormsModule, typeof ɵngcc4.ReactiveFormsModule, typeof ɵngcc5.SpinnerModule, typeof ɵngcc6.I18nModule, typeof ɵngcc7.FormErrorsModule], [typeof ɵngcc1.UpdateProfileComponent, typeof ɵngcc2.UpdateProfileFormComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<UpdateProfileModule>;
 }
-
-//# sourceMappingURL=update-profile.module.d.ts.map
--- cms-structure/pwa/components/add-to-home-screen-banner/add-to-home-screen-banner.component.d.ts.__ivy_ngcc_bak
+++
@@ -1,6 +0,0 @@
-import { AddToHomeScreenService } from '../../services/add-to-home-screen.service';
-import { AddToHomeScreenComponent } from '../add-to-home-screen.component';
-export declare class AddToHomeScreenBannerComponent extends AddToHomeScreenComponent {
-    protected addToHomeScreenService: AddToHomeScreenService;
-    constructor(addToHomeScreenService: AddToHomeScreenService);
-}
