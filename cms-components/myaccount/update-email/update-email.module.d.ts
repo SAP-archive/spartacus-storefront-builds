940c356c002e04b8591d53c530339917432b10a3
GitHub <noreply@github.com>
2020-11-05 17:38:03 +0000

    fae6c00b2 feat: Add specific selectors to all organization components #9466 (#9621)

--- cms-components/myaccount/update-email/update-email.module.d.ts
+++ cms-components/myaccount/update-email/update-email.module.d.ts
@@ -1,2 +1,14 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './update-email-form/update-email-form.component';
+import * as ɵngcc2 from './update-email.component';
+import * as ɵngcc3 from '@angular/common';
+import * as ɵngcc4 from '@angular/forms';
+import * as ɵngcc5 from '../../../shared/components/spinner/spinner.module';
+import * as ɵngcc6 from '@spartacus/core';
+import * as ɵngcc7 from '../../../shared/components/form/form-errors/form-errors.module';
 export declare class UpdateEmailModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<UpdateEmailModule, [typeof ɵngcc1.UpdateEmailFormComponent, typeof ɵngcc2.UpdateEmailComponent], [typeof ɵngcc3.CommonModule, typeof ɵngcc4.FormsModule, typeof ɵngcc4.ReactiveFormsModule, typeof ɵngcc5.SpinnerModule, typeof ɵngcc6.I18nModule, typeof ɵngcc7.FormErrorsModule], [typeof ɵngcc2.UpdateEmailComponent, typeof ɵngcc1.UpdateEmailFormComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<UpdateEmailModule>;
 }
+
+//# sourceMappingURL=update-email.module.d.ts.map
