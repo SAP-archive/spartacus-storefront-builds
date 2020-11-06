ae1f710b3217ced33206a5049c0ee3a159c545fb
GitHub <noreply@github.com>
2020-11-06 14:38:33 +0000

    cc717649d remove CSS transition from focus (#9663)

--- cms-structure/outlet/outlet-ref/outlet-ref.module.d.ts
+++ cms-structure/outlet/outlet-ref/outlet-ref.module.d.ts
@@ -1,9 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './outlet-ref.directive';
-import * as ɵngcc2 from '@angular/common';
 export declare class OutletRefModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<OutletRefModule, [typeof ɵngcc1.OutletRefDirective], [typeof ɵngcc2.CommonModule], [typeof ɵngcc1.OutletRefDirective]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<OutletRefModule>;
 }
-
-//# sourceMappingURL=outlet-ref.module.d.ts.map
