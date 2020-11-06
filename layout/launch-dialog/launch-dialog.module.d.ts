dc0ac51b37dc8aa633d7f137a8ec5c267f00039e
GitHub <noreply@github.com>
2020-11-06 21:23:09 +0000

    fdc02a983 chore: breakpoint service deprecation migration (#9669)

--- layout/launch-dialog/launch-dialog.module.d.ts
+++ layout/launch-dialog/launch-dialog.module.d.ts
@@ -1,4 +1,9 @@
 import { ModuleWithProviders } from '@angular/core';
+import * as ɵngcc0 from '@angular/core';
 export declare class LaunchDialogModule {
     static forRoot(): ModuleWithProviders<LaunchDialogModule>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<LaunchDialogModule, never, never, never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<LaunchDialogModule>;
 }
+
+//# sourceMappingURL=launch-dialog.module.d.ts.map
