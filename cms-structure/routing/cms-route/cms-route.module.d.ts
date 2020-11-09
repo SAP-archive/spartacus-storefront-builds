684744a42246ba58069789b3e995062bd930716d
GitHub <noreply@github.com>
2020-11-09 15:40:20 +0000

    a002a14ef fix: error while saving/updating unit address

--- cms-structure/routing/cms-route/cms-route.module.d.ts
+++ cms-structure/routing/cms-route/cms-route.module.d.ts
@@ -1,2 +1,7 @@
+import * as ɵngcc0 from '@angular/core';
 export declare class CmsRouteModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CmsRouteModule, never, never, never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<CmsRouteModule>;
 }
+
+//# sourceMappingURL=cms-route.module.d.ts.map
