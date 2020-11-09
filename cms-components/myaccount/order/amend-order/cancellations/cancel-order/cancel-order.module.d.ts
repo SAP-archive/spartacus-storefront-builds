f5404793042a6cfc40857bfaa17f2921dd88d97a
GitHub <noreply@github.com>
2020-11-09 17:46:56 +0000

    2b88131d9 fix: Unable to Sort MyCompany Users by Name or Unit #9557 (#9710)

--- cms-components/myaccount/order/amend-order/cancellations/cancel-order/cancel-order.module.d.ts
+++ cms-components/myaccount/order/amend-order/cancellations/cancel-order/cancel-order.module.d.ts
@@ -1,2 +1,13 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './cancel-order.component';
+import * as ɵngcc2 from '@angular/common';
+import * as ɵngcc3 from '@angular/router';
+import * as ɵngcc4 from '../../amend-order-items/amend-order-items.module';
+import * as ɵngcc5 from '../../amend-order-actions/amend-order-actions.module';
+import * as ɵngcc6 from '../../../../../../shared/components/form/form-errors/form-errors.module';
 export declare class CancelOrderModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CancelOrderModule, [typeof ɵngcc1.CancelOrderComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.RouterModule, typeof ɵngcc4.AmendOrderItemsModule, typeof ɵngcc5.AmendOrderActionsModule, typeof ɵngcc6.FormErrorsModule], [typeof ɵngcc1.CancelOrderComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<CancelOrderModule>;
 }
+
+//# sourceMappingURL=cancel-order.module.d.ts.map
