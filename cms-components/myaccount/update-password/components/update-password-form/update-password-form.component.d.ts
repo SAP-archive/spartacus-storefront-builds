1a15855ef7a535090c39aa54376a492c83699560
GitHub <noreply@github.com>
2020-11-09 16:54:21 +0000

    296fa04ac feat: Add specific selectors to all organization table-cell components #9672 (#9685)

--- cms-components/myaccount/update-password/components/update-password-form/update-password-form.component.d.ts
+++ cms-components/myaccount/update-password/components/update-password-form/update-password-form.component.d.ts
@@ -1,6 +1,5 @@
 import { EventEmitter, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import * as ɵngcc0 from '@angular/core';
 export declare class UpdatePasswordFormComponent implements OnInit {
     private fb;
     updatePasswordForm: FormGroup;
@@ -13,8 +12,4 @@ export declare class UpdatePasswordFormComponent implements OnInit {
     ngOnInit(): void;
     onSubmit(): void;
     onCancel(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<UpdatePasswordFormComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<UpdatePasswordFormComponent, "cx-update-password-form", never, {}, { "submitted": "submitted"; "cancelled": "cancelled"; }, never, never>;
 }
-
-//# sourceMappingURL=update-password-form.component.d.ts.map
