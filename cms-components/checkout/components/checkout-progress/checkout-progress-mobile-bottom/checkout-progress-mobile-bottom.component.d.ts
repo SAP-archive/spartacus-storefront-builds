86ff6669d78454398a19355fc35b9f7876815fdb
GitHub <noreply@github.com>
2020-11-10 16:51:44 +0000

    023120686 Fix: Added label for out of stock.(#8923). (#9661)

--- cms-components/checkout/components/checkout-progress/checkout-progress-mobile-bottom/checkout-progress-mobile-bottom.component.d.ts
+++ cms-components/checkout/components/checkout-progress/checkout-progress-mobile-bottom/checkout-progress-mobile-bottom.component.d.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
 import { CheckoutStep } from '../../../model/checkout-step.model';
 import { CheckoutStepService } from '../../../services/checkout-step.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class CheckoutProgressMobileBottomComponent {
     protected checkoutStepService: CheckoutStepService;
     private _steps$;
@@ -8,4 +9,8 @@ export declare class CheckoutProgressMobileBottomComponent {
     activeStepIndex: number;
     activeStepIndex$: Observable<number>;
     get steps$(): Observable<CheckoutStep[]>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<CheckoutProgressMobileBottomComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<CheckoutProgressMobileBottomComponent, "cx-checkout-progress-mobile-bottom", never, {}, {}, never, never>;
 }
+
+//# sourceMappingURL=checkout-progress-mobile-bottom.component.d.ts.map
