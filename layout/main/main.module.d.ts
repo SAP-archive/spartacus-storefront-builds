8f4751d96382719ee256f8c45d1981fded3eb7e9
GitHub <noreply@github.com>
2020-11-10 11:08:15 +0000

    5c29b556d fix: Can't use spartacus after logout on organization page #9704 #9716

--- layout/main/main.module.d.ts
+++ layout/main/main.module.d.ts
@@ -1,21 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './storefront.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@angular/router';
-import * as ɵngcc4 from '../../cms-components/misc/global-message/global-message.module';
-import * as ɵngcc5 from '../../cms-structure/outlet/outlet.module';
-import * as ɵngcc6 from '../../cms-structure/outlet/outlet-ref/outlet-ref.module';
-import * as ɵngcc7 from '../../cms-structure/pwa/pwa.module';
-import * as ɵngcc8 from '../../cms-structure/page/page-layout/page-layout.module';
-import * as ɵngcc9 from '../../cms-structure/seo/seo.module';
-import * as ɵngcc10 from '../../cms-structure/page/slot/page-slot.module';
-import * as ɵngcc11 from '../../shared/components/anonymous-consents-dialog/anonymous-consents-dialog.module';
-import * as ɵngcc12 from '@spartacus/core';
-import * as ɵngcc13 from '../a11y/skip-link/skip-link.module';
-import * as ɵngcc14 from '../a11y/keyboard-focus/keyboard-focus.module';
 export declare class MainModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<MainModule, [typeof ɵngcc1.StorefrontComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.RouterModule, typeof ɵngcc4.GlobalMessageComponentModule, typeof ɵngcc5.OutletModule, typeof ɵngcc6.OutletRefModule, typeof ɵngcc7.PwaModule, typeof ɵngcc8.PageLayoutModule, typeof ɵngcc9.SeoModule, typeof ɵngcc10.PageSlotModule, typeof ɵngcc11.AnonymousConsentsDialogModule, typeof ɵngcc12.FeaturesConfigModule, typeof ɵngcc13.SkipLinkModule, typeof ɵngcc14.KeyboardFocusModule], [typeof ɵngcc1.StorefrontComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<MainModule>;
 }
-
-//# sourceMappingURL=main.module.d.ts.map
