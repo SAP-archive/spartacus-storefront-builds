61f0d274464749fa6199e3e4bb7582b5d30490e2
GitHub <noreply@github.com>
2020-11-10 19:16:48 +0000

    b3a6f58bc chore: Breaking change in product reference component from #8223 (GH-9696)

--- shared/components/media/media.module.d.ts
+++ shared/components/media/media.module.d.ts
@@ -1,11 +1,4 @@
 import { ModuleWithProviders } from '@angular/core';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './media.component';
-import * as ɵngcc2 from '@angular/common';
 export declare class MediaModule {
     static forRoot(): ModuleWithProviders<MediaModule>;
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<MediaModule, [typeof ɵngcc1.MediaComponent], [typeof ɵngcc2.CommonModule], [typeof ɵngcc1.MediaComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<MediaModule>;
 }
-
-//# sourceMappingURL=media.module.d.ts.map
