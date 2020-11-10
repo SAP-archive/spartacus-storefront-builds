4bfb701cf5ef8384ebb33cf5ecc0e536dfa70308
GitHub <noreply@github.com>
2020-11-10 17:02:46 +0000

    023120686 Fix: Added label for out of stock.(#8923). (#9661)

--- cms-pages/product-details-page/product-details-page.module.d.ts
+++ cms-pages/product-details-page/product-details-page.module.d.ts
@@ -1,8 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/router';
 export declare class ProductDetailsPageModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ProductDetailsPageModule, never, [typeof ɵngcc1.RouterModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<ProductDetailsPageModule>;
 }
-
-//# sourceMappingURL=product-details-page.module.d.ts.map
