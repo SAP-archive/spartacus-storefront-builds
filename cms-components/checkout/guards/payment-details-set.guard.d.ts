5984035ef6ec034bc7a63d4708328837e218b36a
GitHub <noreply@github.com>
2020-11-12 20:57:22 +0000

    d38ce63cb chore: Deprecation changes for replenishments (#9553)

--- cms-components/checkout/guards/payment-details-set.guard.d.ts
+++ cms-components/checkout/guards/payment-details-set.guard.d.ts
@@ -3,6 +3,7 @@ import { RoutingConfigService } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { CheckoutStepService } from '../services/checkout-step.service';
 import { CheckoutDetailsService } from '../services/checkout-details.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class PaymentDetailsSetGuard implements CanActivate {
     private checkoutDetailsService;
     private checkoutStepService;
@@ -10,4 +11,7 @@ export declare class PaymentDetailsSetGuard implements CanActivate {
     private router;
     constructor(checkoutDetailsService: CheckoutDetailsService, checkoutStepService: CheckoutStepService, routingConfigService: RoutingConfigService, router: Router);
     canActivate(): Observable<boolean | UrlTree>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<PaymentDetailsSetGuard, never>;
 }
+
+//# sourceMappingURL=payment-details-set.guard.d.ts.map
