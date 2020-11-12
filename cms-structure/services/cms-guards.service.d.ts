c4409fa9e7b569dd5fdb3f6fc9639c8b87e28e83
GitHub <noreply@github.com>
2020-11-12 07:28:41 +0000

    1eca7396c chore: Auth/Session Management Deprecations (#9705)

--- cms-structure/services/cms-guards.service.d.ts
+++ cms-structure/services/cms-guards.service.d.ts
@@ -3,9 +3,13 @@ import { RouterStateSnapshot, UrlTree } from '@angular/router';
 import { CmsActivatedRouteSnapshot } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { CmsComponentsService } from './cms-components.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class CmsGuardsService {
     protected cmsComponentsService: CmsComponentsService;
     protected injector: Injector;
     constructor(cmsComponentsService: CmsComponentsService, injector: Injector);
     cmsPageCanActivate(componentTypes: string[], route: CmsActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<CmsGuardsService, never>;
 }
+
+//# sourceMappingURL=cms-guards.service.d.ts.map
