a043b1a741003a2fd40aa117ce21092e1fc6b2e7
GitHub <noreply@github.com>
2020-11-12 14:13:15 +0000

    97fa67191 chore: Translations migration docs update (#9751)

--- cms-components/product/product-tabs/product-attributes/product-attributes.component.d.ts
+++ cms-components/product/product-tabs/product-attributes/product-attributes.component.d.ts
@@ -1,8 +1,13 @@
 import { Product } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { CurrentProductService } from '../../current-product.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class ProductAttributesComponent {
     protected currentProductService: CurrentProductService;
     product$: Observable<Product>;
     constructor(currentProductService: CurrentProductService);
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<ProductAttributesComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<ProductAttributesComponent, "cx-product-attributes", never, {}, {}, never, never>;
 }
+
+//# sourceMappingURL=product-attributes.component.d.ts.map
