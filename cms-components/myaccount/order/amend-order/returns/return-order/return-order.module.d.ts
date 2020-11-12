6e1e4b4bed0723dcad1fa1132fd8340e8efa8cf2
GitHub <noreply@github.com>
2020-11-12 09:53:23 +0000

    d0eb2e821 feat: Move legacy CMS component loading to optional adapter (#9749)

--- cms-components/myaccount/order/amend-order/returns/return-order/return-order.module.d.ts
+++ cms-components/myaccount/order/amend-order/returns/return-order/return-order.module.d.ts
@@ -1,12 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './return-order.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@angular/router';
-import * as ɵngcc4 from '../../amend-order-items/amend-order-items.module';
-import * as ɵngcc5 from '../../amend-order-actions/amend-order-actions.module';
 export declare class ReturnOrderModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ReturnOrderModule, [typeof ɵngcc1.ReturnOrderComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.RouterModule, typeof ɵngcc4.AmendOrderItemsModule, typeof ɵngcc5.AmendOrderActionsModule], [typeof ɵngcc1.ReturnOrderComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<ReturnOrderModule>;
 }
-
-//# sourceMappingURL=return-order.module.d.ts.map
