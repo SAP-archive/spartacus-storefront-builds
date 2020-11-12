5318e06a03ee42f60c625f0f81f5d0b4baa528d0
GitHub <noreply@github.com>
2020-11-12 14:22:52 +0000

    97fa67191 chore: Translations migration docs update (#9751)

--- cms-components/checkout/guards/checkout-details-loaded.guard.d.ts
+++ cms-components/checkout/guards/checkout-details-loaded.guard.d.ts
@@ -1,12 +1,8 @@
 import { CanActivate } from '@angular/router';
 import { Observable } from 'rxjs';
 import { CheckoutDetailsService } from '../services/checkout-details.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class CheckoutDetailsLoadedGuard implements CanActivate {
     private checkoutDetailsService;
     constructor(checkoutDetailsService: CheckoutDetailsService);
     canActivate(): Observable<boolean>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CheckoutDetailsLoadedGuard, never>;
 }
-
-//# sourceMappingURL=checkout-details-loaded.guard.d.ts.map
