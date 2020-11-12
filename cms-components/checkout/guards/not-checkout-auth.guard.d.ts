22ea3c51f049f6370de69095cfec903f7299095a
GitHub <noreply@github.com>
2020-11-12 12:27:17 +0000

    fb8b74d55 chore: migration for StockNotificationComponent (#9730)

--- cms-components/checkout/guards/not-checkout-auth.guard.d.ts
+++ cms-components/checkout/guards/not-checkout-auth.guard.d.ts
@@ -1,7 +1,6 @@
 import { CanActivate, Router, UrlTree } from '@angular/router';
 import { ActiveCartService, AuthService, SemanticPathService } from '@spartacus/core';
 import { Observable } from 'rxjs';
-import * as ɵngcc0 from '@angular/core';
 export declare class NotCheckoutAuthGuard implements CanActivate {
     protected authService: AuthService;
     protected activeCartService: ActiveCartService;
@@ -9,7 +8,4 @@ export declare class NotCheckoutAuthGuard implements CanActivate {
     protected router: Router;
     constructor(authService: AuthService, activeCartService: ActiveCartService, semanticPathService: SemanticPathService, router: Router);
     canActivate(): Observable<boolean | UrlTree>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<NotCheckoutAuthGuard, never>;
 }
-
-//# sourceMappingURL=not-checkout-auth.guard.d.ts.map
