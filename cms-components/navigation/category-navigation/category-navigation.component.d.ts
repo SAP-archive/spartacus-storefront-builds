5793c31ae1ce330bd951da9d3ed3ec57804590f5
GitHub <noreply@github.com>
2020-11-12 11:48:33 +0000

    217354e3b chore: migration for removed properties of LoginFormComponent and removed redirect behavior (#9732)

--- cms-components/navigation/category-navigation/category-navigation.component.d.ts
+++ cms-components/navigation/category-navigation/category-navigation.component.d.ts
@@ -3,15 +3,10 @@ import { Observable } from 'rxjs';
 import { CmsComponentData } from '../../../cms-structure/page/model/cms-component-data';
 import { NavigationNode } from '../navigation/navigation-node.model';
 import { NavigationService } from '../navigation/navigation.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class CategoryNavigationComponent {
     protected componentData: CmsComponentData<CmsNavigationComponent>;
     protected service: NavigationService;
     node$: Observable<NavigationNode>;
     data$: Observable<CmsNavigationComponent>;
     constructor(componentData: CmsComponentData<CmsNavigationComponent>, service: NavigationService);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CategoryNavigationComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<CategoryNavigationComponent, "cx-category-navigation", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=category-navigation.component.d.ts.map
