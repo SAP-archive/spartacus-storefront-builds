f9728e4f7b54b7b4dbdea7b6e7d37fead090802b
GitHub <noreply@github.com>
2020-11-12 21:08:09 +0000

    d38ce63cb chore: Deprecation changes for replenishments (#9553)

--- shared/components/generic-link/generic-link.module.d.ts
+++ shared/components/generic-link/generic-link.module.d.ts
@@ -1,10 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './generic-link.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@angular/router';
 export declare class GenericLinkModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<GenericLinkModule, [typeof ɵngcc1.GenericLinkComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.RouterModule], [typeof ɵngcc1.GenericLinkComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<GenericLinkModule>;
 }
-
-//# sourceMappingURL=generic-link.module.d.ts.map
