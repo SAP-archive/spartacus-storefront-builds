fc0b1d6a3cbe20c45955597ecbb8d5b42149e1b5
GitHub <noreply@github.com>
2020-11-13 17:32:53 +0000

    d2caac5cc chore: avoid double Travis job for PRs from feature branch to develop (#9778)

--- cms-components/checkout/components/review-submit/review-submit.module.d.ts
+++ cms-components/checkout/components/review-submit/review-submit.module.d.ts
@@ -1,15 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './review-submit.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '../../../../shared/components/card/card.module';
-import * as ɵngcc4 from '../../../cart/cart-shared/cart-shared.module';
-import * as ɵngcc5 from '@spartacus/core';
-import * as ɵngcc6 from '@angular/router';
-import * as ɵngcc7 from '../promotions/promotions.module';
-import * as ɵngcc8 from '../../../misc/icon/icon.module';
 export declare class ReviewSubmitModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ReviewSubmitModule, [typeof ɵngcc1.ReviewSubmitComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.CardModule, typeof ɵngcc4.CartSharedModule, typeof ɵngcc5.I18nModule, typeof ɵngcc5.UrlModule, typeof ɵngcc6.RouterModule, typeof ɵngcc7.PromotionsModule, typeof ɵngcc8.IconModule], [typeof ɵngcc1.ReviewSubmitComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<ReviewSubmitModule>;
 }
-
-//# sourceMappingURL=review-submit.module.d.ts.map
