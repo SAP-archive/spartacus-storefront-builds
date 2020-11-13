ffd6599ed68529404841eaa6578c92b3c19b9748
GitHub <noreply@github.com>
2020-11-13 14:59:55 +0000

    4f8deb5e1 feat: inform profiletag when a consent has been revoked. (GH-9474)

--- layout/a11y/keyboard-focus/block/block-focus.directive.d.ts
+++ layout/a11y/keyboard-focus/block/block-focus.directive.d.ts
@@ -2,7 +2,6 @@ import { ElementRef, OnInit } from '@angular/core';
 import { BaseFocusService } from '../base/base-focus.service';
 import { BlockFocusConfig } from '../keyboard-focus.model';
 import { VisibleFocusDirective } from '../visible/visible-focus.directive';
-import * as ɵngcc0 from '@angular/core';
 export declare class BlockFocusDirective extends VisibleFocusDirective implements OnInit {
     protected elementRef: ElementRef;
     protected service: BaseFocusService;
@@ -10,8 +9,4 @@ export declare class BlockFocusDirective extends VisibleFocusDirective implement
     protected config: BlockFocusConfig;
     constructor(elementRef: ElementRef, service: BaseFocusService);
     ngOnInit(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<BlockFocusDirective, never>;
-    static ɵdir: ɵngcc0.ɵɵDirectiveDefWithMeta<BlockFocusDirective, never, never, {}, {}, never>;
 }
-
-//# sourceMappingURL=block-focus.directive.d.ts.map
