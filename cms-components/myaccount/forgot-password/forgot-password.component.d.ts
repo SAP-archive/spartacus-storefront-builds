68d07cac94c84abb0f55c96b6e929d484cb62b23
GitHub <noreply@github.com>
2020-11-13 15:37:29 +0000

    0cb1f40f2 fix: Approvers list changes based on what list you checked before #9465 (#9761)

--- cms-components/myaccount/forgot-password/forgot-password.component.d.ts
+++ cms-components/myaccount/forgot-password/forgot-password.component.d.ts
@@ -1,7 +1,6 @@
 import { OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthConfigService, RoutingService, UserService } from '@spartacus/core';
-import * as ɵngcc0 from '@angular/core';
 export declare class ForgotPasswordComponent implements OnInit {
     protected fb: FormBuilder;
     protected userService: UserService;
@@ -11,8 +10,4 @@ export declare class ForgotPasswordComponent implements OnInit {
     constructor(fb: FormBuilder, userService: UserService, routingService: RoutingService, authConfigService: AuthConfigService);
     ngOnInit(): void;
     requestForgotPasswordEmail(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<ForgotPasswordComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<ForgotPasswordComponent, "cx-forgot-password", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=forgot-password.component.d.ts.map
