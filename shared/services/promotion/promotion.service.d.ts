6f4262ddd2a99a9c94ed9d44ab65f55140acfa2c
GitHub <noreply@github.com>
2020-11-13 12:14:58 +0000

    34e8abf41 Auth/session management deprecations (#9752)

--- shared/services/promotion/promotion.service.d.ts
+++ shared/services/promotion/promotion.service.d.ts
@@ -1,7 +1,6 @@
 import { ActiveCartService, CheckoutService, OrderEntry, PromotionLocation, PromotionResult } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { OrderDetailsService } from '../../../cms-components/myaccount/order/order-details/order-details.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class PromotionService {
     protected orderDetailsService: OrderDetailsService;
     protected checkoutService: CheckoutService;
@@ -16,7 +15,4 @@ export declare class PromotionService {
     getProductPromotionForEntry(item: OrderEntry, promotionLocation: PromotionLocation): Observable<PromotionResult[]>;
     private getProductPromotion;
     private isConsumedByEntry;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<PromotionService, never>;
 }
-
-//# sourceMappingURL=promotion.service.d.ts.map
