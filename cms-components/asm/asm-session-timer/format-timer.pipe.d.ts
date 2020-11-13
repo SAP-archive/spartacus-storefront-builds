4d57879bf2f3fcf935b69651a6bb0f7853b23f44
GitHub <noreply@github.com>
2020-11-13 13:06:58 +0000

    22d96f589 chore: run Travis for partial PRs to epic and feature branches (#9772)

--- cms-components/asm/asm-session-timer/format-timer.pipe.d.ts
+++ cms-components/asm/asm-session-timer/format-timer.pipe.d.ts
@@ -1,4 +1,9 @@
 import { PipeTransform } from '@angular/core';
+import * as ɵngcc0 from '@angular/core';
 export declare class FormatTimerPipe implements PipeTransform {
     transform(totalSeconds: number): string;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<FormatTimerPipe, never>;
+    static ɵpipe: ɵngcc0.ɵɵPipeDefWithMeta<FormatTimerPipe, "formatTimer">;
 }
+
+//# sourceMappingURL=format-timer.pipe.d.ts.map
---
+++ cms-components/product/product-tabs/product-attributes/product-attributes.component.d.ts.__ivy_ngcc_bak
@@ -0,0 +1,8 @@
+import { Product } from '@spartacus/core';
+import { Observable } from 'rxjs';
+import { CurrentProductService } from '../../current-product.service';
+export declare class ProductAttributesComponent {
+    protected currentProductService: CurrentProductService;
+    product$: Observable<Product>;
+    constructor(currentProductService: CurrentProductService);
+}
