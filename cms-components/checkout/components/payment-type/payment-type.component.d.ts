fcb78e4e9d6ce17a7945641300a4ba2b87e4fe3f
GitHub <noreply@github.com>
2020-11-13 11:27:52 +0000

    d0bb621c1 chore: Migrations for AuthModule, AsmModule, KymaModule and CDC. (#9744)

--- cms-components/checkout/components/payment-type/payment-type.component.d.ts
+++ cms-components/checkout/components/payment-type/payment-type.component.d.ts
@@ -2,6 +2,7 @@ import { ActivatedRoute } from '@angular/router';
 import { PaymentType, PaymentTypeService } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { CheckoutStepService } from '../../services/checkout-step.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class PaymentTypeComponent {
     protected paymentTypeService: PaymentTypeService;
     protected checkoutStepService: CheckoutStepService;
@@ -16,4 +17,8 @@ export declare class PaymentTypeComponent {
     changeType(code: string): void;
     next(): void;
     back(): void;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<PaymentTypeComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<PaymentTypeComponent, "cx-payment-type", never, {}, {}, never, never>;
 }
+
+//# sourceMappingURL=payment-type.component.d.ts.map
