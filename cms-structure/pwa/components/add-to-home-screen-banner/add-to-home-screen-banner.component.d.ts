e9eab4c61901590e89ba10dd53cab990094539da
GitHub <noreply@github.com>
2020-11-16 10:46:48 +0000

    4f032a365 feat: replace `storageSync` with `state persistence service` (#9715)

--- cms-structure/pwa/components/add-to-home-screen-banner/add-to-home-screen-banner.component.d.ts
+++ cms-structure/pwa/components/add-to-home-screen-banner/add-to-home-screen-banner.component.d.ts
@@ -1,11 +1,6 @@
 import { AddToHomeScreenService } from '../../services/add-to-home-screen.service';
 import { AddToHomeScreenComponent } from '../add-to-home-screen.component';
-import * as ɵngcc0 from '@angular/core';
 export declare class AddToHomeScreenBannerComponent extends AddToHomeScreenComponent {
     protected addToHomeScreenService: AddToHomeScreenService;
     constructor(addToHomeScreenService: AddToHomeScreenService);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<AddToHomeScreenBannerComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<AddToHomeScreenBannerComponent, "cx-add-to-home-screen-banner", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=add-to-home-screen-banner.component.d.ts.map
