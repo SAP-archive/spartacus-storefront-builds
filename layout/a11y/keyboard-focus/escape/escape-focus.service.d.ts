52d96cf03556a003a72dcedb5e010378d9d8bdd2
GitHub <noreply@github.com>
2020-11-16 11:58:12 +0000

    3af91717c feat: rename B2BUserGroup -> B2BUserRole (#9764)

--- layout/a11y/keyboard-focus/escape/escape-focus.service.d.ts
+++ layout/a11y/keyboard-focus/escape/escape-focus.service.d.ts
@@ -1,13 +1,9 @@
 import { EscapeFocusConfig } from '../keyboard-focus.model';
 import { PersistFocusService } from '../persist/persist-focus.service';
 import { SelectFocusUtility } from '../services/select-focus.util';
-import * as ɵngcc0 from '@angular/core';
 export declare class EscapeFocusService extends PersistFocusService {
     protected selectFocusUtil: SelectFocusUtility;
     constructor(selectFocusUtil: SelectFocusUtility);
     shouldFocus(config: EscapeFocusConfig): boolean;
     handleEscape(host: HTMLElement, config: EscapeFocusConfig, event: KeyboardEvent): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<EscapeFocusService, never>;
 }
-
-//# sourceMappingURL=escape-focus.service.d.ts.map
