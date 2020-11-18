44761043290dc4ae27679e88d0d797ab7e197c4f
GitHub <noreply@github.com>
2020-11-18 11:37:52 +0000

    b59cdd248 feat: refactor - prefix all organization/administration components' selectors with cx-org #9484 (#9826)

--- cms-components/asm/services/asm-component.service.d.ts
+++ cms-components/asm/services/asm-component.service.d.ts
@@ -1,6 +1,5 @@
 import { AuthService, CsAgentAuthService, WindowRef } from '@spartacus/core';
 import { Observable } from 'rxjs';
-import * as ɵngcc0 from '@angular/core';
 export declare class AsmComponentService {
     protected authService: AuthService;
     protected csAgentAuthService: CsAgentAuthService;
@@ -17,7 +16,4 @@ export declare class AsmComponentService {
      * - remove the generated UI from the DOM (outlets currently do not support this)
      */
     unload(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<AsmComponentService, never>;
 }
-
-//# sourceMappingURL=asm-component.service.d.ts.map
