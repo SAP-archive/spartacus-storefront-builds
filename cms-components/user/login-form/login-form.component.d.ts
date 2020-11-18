3ca8c5c65eac7b530cd35978f79f8bb7ca496dee
GitHub <noreply@github.com>
2020-11-18 20:01:29 +0000

    0c73bc555 fix: organization schematics - replace B2cStorefrontModule with B2bStorefrontModule (#9836)

--- cms-components/user/login-form/login-form.component.d.ts
+++ cms-components/user/login-form/login-form.component.d.ts
@@ -1,6 +1,7 @@
 import { OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthService, GlobalMessageService, WindowRef } from '@spartacus/core';
+import * as ɵngcc0 from '@angular/core';
 export declare class LoginFormComponent implements OnInit {
     protected auth: AuthService;
     protected globalMessageService: GlobalMessageService;
@@ -11,4 +12,8 @@ export declare class LoginFormComponent implements OnInit {
     ngOnInit(): void;
     submitForm(): void;
     protected loginUser(): void;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<LoginFormComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<LoginFormComponent, "cx-login-form", never, {}, {}, never, never>;
 }
+
+//# sourceMappingURL=login-form.component.d.ts.map
