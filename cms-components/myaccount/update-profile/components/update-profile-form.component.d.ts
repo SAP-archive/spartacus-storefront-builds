60cfb6af6226c2dcc86570f5e1c1c54c821018da
GitHub <noreply@github.com>
2020-11-18 17:21:55 +0000

    9d7f2b327 fix: remove search box when hamburger menu is open (#9520)

--- cms-components/myaccount/update-profile/components/update-profile-form.component.d.ts
+++ cms-components/myaccount/update-profile/components/update-profile-form.component.d.ts
@@ -1,6 +1,7 @@
 import { EventEmitter, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Title, User } from '@spartacus/core';
+import * as ɵngcc0 from '@angular/core';
 export declare class UpdateProfileFormComponent implements OnInit {
     private fb;
     user: User;
@@ -14,4 +15,8 @@ export declare class UpdateProfileFormComponent implements OnInit {
     ngOnInit(): void;
     onSubmit(): void;
     onCancel(): void;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<UpdateProfileFormComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<UpdateProfileFormComponent, "cx-update-profile-form", never, { "user": "user"; "titles": "titles"; }, { "submitted": "submitted"; "cancelled": "cancelled"; }, never, never>;
 }
+
+//# sourceMappingURL=update-profile-form.component.d.ts.map
