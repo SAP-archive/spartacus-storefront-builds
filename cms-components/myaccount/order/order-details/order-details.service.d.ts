4deb2dde23ddf3a2eb92401ce6822c9f5aca6013
GitHub <noreply@github.com>
2020-11-18 12:10:30 +0000

    fe13041df fix: state field value while editing shipping address

--- cms-components/myaccount/order/order-details/order-details.service.d.ts
+++ cms-components/myaccount/order/order-details/order-details.service.d.ts
@@ -1,6 +1,5 @@
 import { Order, RoutingService, UserOrderService } from '@spartacus/core';
 import { Observable } from 'rxjs';
-import * as ɵngcc0 from '@angular/core';
 export declare class OrderDetailsService {
     private userOrderService;
     private routingService;
@@ -8,7 +7,4 @@ export declare class OrderDetailsService {
     orderLoad$: Observable<{}>;
     constructor(userOrderService: UserOrderService, routingService: RoutingService);
     getOrderDetails(): Observable<Order>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<OrderDetailsService, never>;
 }
-
-//# sourceMappingURL=order-details.service.d.ts.map
