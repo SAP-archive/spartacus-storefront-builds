2595305915085e4a8df9e2d44691232133cdcbf3
GitHub <noreply@github.com>
2020-11-19 13:17:26 +0000

    3a9d4a61f feat: add storefinder ng-add and migration schematics (#9914)

--- cms-components/cart/cart-details/cart-details.module.d.ts
+++ cms-components/cart/cart-details/cart-details.module.d.ts
@@ -1,14 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './cart-details.component';
-import * as ɵngcc2 from '../cart-shared/cart-shared.module';
-import * as ɵngcc3 from '@angular/common';
-import * as ɵngcc4 from '../cart-coupon/cart-coupon.module';
-import * as ɵngcc5 from '@angular/router';
-import * as ɵngcc6 from '@spartacus/core';
-import * as ɵngcc7 from '../../checkout/components/promotions/promotions.module';
 export declare class CartDetailsModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CartDetailsModule, [typeof ɵngcc1.CartDetailsComponent], [typeof ɵngcc2.CartSharedModule, typeof ɵngcc3.CommonModule, typeof ɵngcc4.CartCouponModule, typeof ɵngcc5.RouterModule, typeof ɵngcc6.UrlModule, typeof ɵngcc7.PromotionsModule, typeof ɵngcc6.FeaturesConfigModule, typeof ɵngcc6.I18nModule], [typeof ɵngcc1.CartDetailsComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<CartDetailsModule>;
 }
-
-//# sourceMappingURL=cart-details.module.d.ts.map
