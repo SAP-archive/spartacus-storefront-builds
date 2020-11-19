0d0c747ef8819a8db39c7446cdd102994908f34e
GitHub <noreply@github.com>
2020-11-19 15:10:07 +0000

    35b614acf chore: changes regarding migrations

--- cms-components/product/product-tabs/product-reviews/product-reviews.module.d.ts
+++ cms-components/product/product-tabs/product-reviews/product-reviews.module.d.ts
@@ -1,13 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './product-reviews.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@angular/forms';
-import * as ɵngcc4 from '@spartacus/core';
-import * as ɵngcc5 from '../../../../shared/components/star-rating/star-rating.module';
-import * as ɵngcc6 from '../../../../shared/components/form/form-errors/form-errors.module';
 export declare class ProductReviewsModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ProductReviewsModule, [typeof ɵngcc1.ProductReviewsComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.ReactiveFormsModule, typeof ɵngcc3.FormsModule, typeof ɵngcc4.I18nModule, typeof ɵngcc5.StarRatingModule, typeof ɵngcc6.FormErrorsModule], [typeof ɵngcc1.ProductReviewsComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<ProductReviewsModule>;
 }
-
-//# sourceMappingURL=product-reviews.module.d.ts.map
