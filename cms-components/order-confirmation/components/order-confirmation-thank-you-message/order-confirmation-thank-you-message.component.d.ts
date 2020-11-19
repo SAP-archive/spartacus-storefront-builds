3ccd210e3e611941183b9c0fd39f1e5c4ce72406
GitHub <noreply@github.com>
2020-11-19 13:04:21 +0000

    3a9d4a61f feat: add storefinder ng-add and migration schematics (#9914)

--- cms-components/order-confirmation/components/order-confirmation-thank-you-message/order-confirmation-thank-you-message.component.d.ts
+++ cms-components/order-confirmation/components/order-confirmation-thank-you-message/order-confirmation-thank-you-message.component.d.ts
@@ -1,6 +1,7 @@
 import { OnDestroy, OnInit } from '@angular/core';
 import { CheckoutService } from '@spartacus/core';
 import { Observable } from 'rxjs';
+import * as ɵngcc0 from '@angular/core';
 export declare class OrderConfirmationThankYouMessageComponent implements OnInit, OnDestroy {
     protected checkoutService: CheckoutService;
     order$: Observable<any>;
@@ -10,4 +11,8 @@ export declare class OrderConfirmationThankYouMessageComponent implements OnInit
     constructor(checkoutService: CheckoutService);
     ngOnInit(): void;
     ngOnDestroy(): void;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<OrderConfirmationThankYouMessageComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<OrderConfirmationThankYouMessageComponent, "cx-order-confirmation-thank-you-message", never, {}, {}, never, never>;
 }
+
+//# sourceMappingURL=order-confirmation-thank-you-message.component.d.ts.map
