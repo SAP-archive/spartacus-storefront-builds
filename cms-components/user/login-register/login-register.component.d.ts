f72d5edb89a648d27ee718ccb525b6d9f2b4ba4b
GitHub <noreply@github.com>
2020-11-20 11:54:23 +0000

    a406e15be test: e2e - check links of every organization item (#9917)

--- cms-components/user/login-register/login-register.component.d.ts
+++ cms-components/user/login-register/login-register.component.d.ts
@@ -1,15 +1,10 @@
 import { OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CheckoutConfigService } from '../../checkout/services/index';
-import * as ɵngcc0 from '@angular/core';
 export declare class LoginRegisterComponent implements OnInit {
     protected checkoutConfigService: CheckoutConfigService;
     protected activatedRoute: ActivatedRoute;
     loginAsGuest: boolean;
     constructor(checkoutConfigService: CheckoutConfigService, activatedRoute: ActivatedRoute);
     ngOnInit(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<LoginRegisterComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<LoginRegisterComponent, "cx-login-register", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=login-register.component.d.ts.map
