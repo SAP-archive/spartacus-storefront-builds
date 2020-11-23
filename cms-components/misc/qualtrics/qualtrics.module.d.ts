8bc622440bee8e8da6cce24087604103155570b1
GitHub <noreply@github.com>
2020-11-23 00:23:24 +0000

    f65b6b8ae fix: Restricting access to approval dashboard to unauthorized users (#9738)

--- cms-components/misc/qualtrics/qualtrics.module.d.ts
+++ cms-components/misc/qualtrics/qualtrics.module.d.ts
@@ -1,9 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './qualtrics.component';
-import * as ɵngcc2 from '@angular/common';
 export declare class QualtricsModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<QualtricsModule, [typeof ɵngcc1.QualtricsComponent], [typeof ɵngcc2.CommonModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<QualtricsModule>;
 }
-
-//# sourceMappingURL=qualtrics.module.d.ts.map
