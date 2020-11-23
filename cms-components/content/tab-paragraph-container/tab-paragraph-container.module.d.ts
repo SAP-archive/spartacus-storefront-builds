c0719523c2d19ad4514ad4f63fe6c9d289dcf031
GitHub <noreply@github.com>
2020-11-23 10:14:39 +0000

    265c29fe4 chore: Update release issue template for misc lib (#9974)

--- cms-components/content/tab-paragraph-container/tab-paragraph-container.module.d.ts
+++ cms-components/content/tab-paragraph-container/tab-paragraph-container.module.d.ts
@@ -1,12 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './tab-paragraph-container.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '../../../cms-structure/page/component/page-component.module';
-import * as ɵngcc4 from '../../../cms-structure/outlet/outlet.module';
-import * as ɵngcc5 from '@spartacus/core';
 export declare class TabParagraphContainerModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<TabParagraphContainerModule, [typeof ɵngcc1.TabParagraphContainerComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.PageComponentModule, typeof ɵngcc4.OutletModule, typeof ɵngcc5.I18nModule], [typeof ɵngcc1.TabParagraphContainerComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<TabParagraphContainerModule>;
 }
-
-//# sourceMappingURL=tab-paragraph-container.module.d.ts.map
