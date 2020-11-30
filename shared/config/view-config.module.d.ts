32dfe1a219bf9a806e7aabd8d74904c2d298208e
GitHub <noreply@github.com>
2020-11-30 08:46:09 +0000

    bbdf82029 feature: New replacement for invokeWithUserId method (#10035)

--- shared/config/view-config.module.d.ts
+++ shared/config/view-config.module.d.ts
@@ -1,9 +1,4 @@
 import { ModuleWithProviders } from '@angular/core';
-import * as ɵngcc0 from '@angular/core';
 export declare class ViewConfigModule {
     static forRoot(): ModuleWithProviders<ViewConfigModule>;
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ViewConfigModule, never, never, never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<ViewConfigModule>;
 }
-
-//# sourceMappingURL=view-config.module.d.ts.map
