bcde8e04ba3faef37a90d0daf1fb5367cc1f40b0
GitHub <noreply@github.com>
2020-12-01 16:44:36 +0000

    2ffd6f233 chore: Move tab test to b2b checkout main tests (#10072)

--- layout/direction/direction.module.d.ts
+++ layout/direction/direction.module.d.ts
@@ -1,8 +1,13 @@
 import { FeatureConfigService } from '@spartacus/core';
 import { DirectionService } from './direction.service';
+import * as ɵngcc0 from '@angular/core';
 export declare function initHtmlDirAttribute(directionService: DirectionService, featureConfigService: FeatureConfigService): () => Promise<void>;
 /**
  * Provides a configuration and APP_INITIALIZER to add the correct (language drive) html direction.
  */
 export declare class DirectionModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<DirectionModule, never, never, never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<DirectionModule>;
 }
+
+//# sourceMappingURL=direction.module.d.ts.map
