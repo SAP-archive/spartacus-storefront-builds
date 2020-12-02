d8623e2d7953dcefcf23503907e39565b65a17cf
GitHub <noreply@github.com>
2020-12-02 14:14:02 +0000

    982fb54ae fix: add translations for the confirmation dialog (#10042)

--- cms-components/content/banner/banner.component.d.ts
+++ cms-components/content/banner/banner.component.d.ts
@@ -1,6 +1,11 @@
 import { CmsBannerComponent } from '@spartacus/core';
 import { CmsComponentData } from '../../../cms-structure/page/model/cms-component-data';
+import * as ɵngcc0 from '@angular/core';
 export declare class BannerComponent {
     component: CmsComponentData<CmsBannerComponent>;
     constructor(component: CmsComponentData<CmsBannerComponent>);
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<BannerComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<BannerComponent, "cx-banner", never, {}, {}, never, never>;
 }
+
+//# sourceMappingURL=banner.component.d.ts.map
