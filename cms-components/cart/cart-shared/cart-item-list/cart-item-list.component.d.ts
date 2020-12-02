ce4f92d705c6b97aaaf5c1caca7920f146674a46
GitHub <noreply@github.com>
2020-12-02 17:16:54 +0000

    cbea06635 Bug: fix safari modal in mobile  (#10166)

--- cms-components/cart/cart-shared/cart-item-list/cart-item-list.component.d.ts
+++ cms-components/cart/cart-shared/cart-item-list/cart-item-list.component.d.ts
@@ -2,7 +2,6 @@ import { FormGroup } from '@angular/forms';
 import { ActiveCartService, OrderEntry, PromotionLocation, SelectiveCartService } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { CartItemComponentOptions } from '../cart-item/cart-item.component';
-import * as ɵngcc0 from '@angular/core';
 export declare class CartItemListComponent {
     protected activeCartService: ActiveCartService;
     protected selectiveCartService: SelectiveCartService;
@@ -25,8 +24,4 @@ export declare class CartItemListComponent {
     protected getControlName(item: OrderEntry): string;
     removeEntry(item: OrderEntry): void;
     getControl(item: OrderEntry): Observable<FormGroup>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CartItemListComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<CartItemListComponent, "cx-cart-item-list", never, { "readonly": "readonly"; "hasHeader": "hasHeader"; "options": "options"; "promotionLocation": "promotionLocation"; "items": "items"; "setLoading": "cartIsLoading"; }, {}, never, never>;
 }
-
-//# sourceMappingURL=cart-item-list.component.d.ts.map
