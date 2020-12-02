a4101a2f908ad1b303c943e7c35b4dd10095c250
GitHub <noreply@github.com>
2020-12-02 12:46:24 +0000

    37d61834c chore: schematics - extract the lib generating mechanism (#10003)

--- shared/components/carousel/carousel.module.d.ts
+++ shared/components/carousel/carousel.module.d.ts
@@ -1,2 +1,13 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './carousel.component';
+import * as ɵngcc2 from '@angular/common';
+import * as ɵngcc3 from '@angular/router';
+import * as ɵngcc4 from '../../../cms-components/misc/icon/icon.module';
+import * as ɵngcc5 from '../media/media.module';
+import * as ɵngcc6 from '@spartacus/core';
 export declare class CarouselModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CarouselModule, [typeof ɵngcc1.CarouselComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.RouterModule, typeof ɵngcc4.IconModule, typeof ɵngcc5.MediaModule, typeof ɵngcc6.UrlModule], [typeof ɵngcc1.CarouselComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<CarouselModule>;
 }
+
+//# sourceMappingURL=carousel.module.d.ts.map
