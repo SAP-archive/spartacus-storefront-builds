485dff410bd7a3b6e42d2ca553ef352aa13be645
GitHub <noreply@github.com>
2020-12-02 14:26:02 +0000

    982fb54ae fix: add translations for the confirmation dialog (#10042)

--- cms-components/order-confirmation/components/order-confirmation-overview/order-confirmation-overview.component.d.ts
+++ cms-components/order-confirmation/components/order-confirmation-overview/order-confirmation-overview.component.d.ts
@@ -1,15 +1,10 @@
 import { OnDestroy, OnInit } from '@angular/core';
 import { CheckoutService } from '@spartacus/core';
 import { Observable } from 'rxjs';
-import * as ɵngcc0 from '@angular/core';
 export declare class OrderConfirmationOverviewComponent implements OnInit, OnDestroy {
     protected checkoutService: CheckoutService;
     order$: Observable<any>;
     constructor(checkoutService: CheckoutService);
     ngOnInit(): void;
     ngOnDestroy(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<OrderConfirmationOverviewComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<OrderConfirmationOverviewComponent, "cx-order-confirmation-overview", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=order-confirmation-overview.component.d.ts.map
