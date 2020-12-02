528fa36873b46c28648c43c550bfe45372cacd12
GitHub <noreply@github.com>
2020-12-02 19:30:08 +0000

    99432c4ae test: Failing e2e b2b\my-account\b2b-replenishment-order-details-flow.e2e-spec.ts (GH-10065)

--- cms-components/cart/cart-coupon/applied-coupons/applied-coupons.component.d.ts
+++ cms-components/cart/cart-coupon/applied-coupons/applied-coupons.component.d.ts
@@ -1,6 +1,5 @@
 import { Voucher, CartVoucherService } from '@spartacus/core';
 import { ICON_TYPE } from '../../../../cms-components/misc/icon/icon.model';
-import * as ɵngcc0 from '@angular/core';
 export declare class AppliedCouponsComponent {
     private cartVoucherService;
     vouchers: Voucher[];
@@ -10,8 +9,4 @@ export declare class AppliedCouponsComponent {
     constructor(cartVoucherService: CartVoucherService);
     get sortedVouchers(): Voucher[];
     removeVoucher(voucherId: string): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<AppliedCouponsComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<AppliedCouponsComponent, "cx-applied-coupons", never, { "cartIsLoading": "cartIsLoading"; "isReadOnly": "isReadOnly"; "vouchers": "vouchers"; }, {}, never, never>;
 }
-
-//# sourceMappingURL=applied-coupons.component.d.ts.map
