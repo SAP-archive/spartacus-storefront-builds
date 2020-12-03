bc141f4b266ad3712e0877d940ab7a86fe05316d
GitHub <noreply@github.com>
2020-12-03 12:05:56 +0000

    b92a7c043 fix: Message service notifications are layered on each other #9798 (#10051)

--- cms-components/product/product-list/product-facet-navigation/product-facet-navigation.module.d.ts
+++ cms-components/product/product-list/product-facet-navigation/product-facet-navigation.module.d.ts
@@ -1,13 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './product-facet-navigation.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from './facet-list/facet-list.module';
-import * as ɵngcc4 from './active-facets/active-facets.module';
-import * as ɵngcc5 from '../../../misc/icon/icon.module';
-import * as ɵngcc6 from '@spartacus/core';
 export declare class ProductFacetNavigationModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ProductFacetNavigationModule, [typeof ɵngcc1.ProductFacetNavigationComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.FacetListModule, typeof ɵngcc4.ActiveFacetsModule, typeof ɵngcc5.IconModule, typeof ɵngcc6.I18nModule, typeof ɵngcc6.ConfigModule], [typeof ɵngcc1.ProductFacetNavigationComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<ProductFacetNavigationModule>;
 }
-
-//# sourceMappingURL=product-facet-navigation.module.d.ts.map
