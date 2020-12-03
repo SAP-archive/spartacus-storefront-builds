0aa7e642c4568cd4d5cb3b3bc7d1f1b860c26919
GitHub <noreply@github.com>
2020-12-03 15:12:03 +0000

    ee7fc8d29 chore: Create METHOD migration for PageMetaService (GH-10139)

--- cms-components/checkout/components/shipping-address/address-form/address-form.component.d.ts
+++ cms-components/checkout/components/shipping-address/address-form/address-form.component.d.ts
@@ -3,7 +3,6 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { Address, AddressValidation, CheckoutDeliveryService, Country, GlobalMessageService, Region, Title, UserAddressService, UserService } from '@spartacus/core';
 import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 import { ModalRef, ModalService } from '../../../../../shared/components/modal/index';
-import * as ɵngcc0 from '@angular/core';
 export declare class AddressFormComponent implements OnInit, OnDestroy {
     protected fb: FormBuilder;
     protected checkoutDeliveryService: CheckoutDeliveryService;
@@ -37,8 +36,4 @@ export declare class AddressFormComponent implements OnInit, OnDestroy {
     verifyAddress(): void;
     openSuggestedAddress(results: AddressValidation): void;
     ngOnDestroy(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<AddressFormComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<AddressFormComponent, "cx-address-form", never, { "setAsDefaultField": "setAsDefaultField"; "showCancelBtn": "showCancelBtn"; "addressData": "addressData"; "actionBtnLabel": "actionBtnLabel"; "cancelBtnLabel": "cancelBtnLabel"; "showTitleCode": "showTitleCode"; }, { "submitAddress": "submitAddress"; "backToAddress": "backToAddress"; }, never, never>;
 }
-
-//# sourceMappingURL=address-form.component.d.ts.map
