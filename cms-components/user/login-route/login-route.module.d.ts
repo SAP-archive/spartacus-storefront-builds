c3d99924ddc81303c38a4271102fec6ff0648ae7
GitHub <noreply@github.com>
2020-12-03 16:48:06 +0000

    3ac31325e chore: Add ${TODO_SPARTACUS} in html comments for component deprecations (GH-10138)(#10193)

--- cms-components/user/login-route/login-route.module.d.ts
+++ cms-components/user/login-route/login-route.module.d.ts
@@ -5,12 +5,5 @@
  *
  * Instead of manually invoking OAuth redirect you only have to redirect to `login` page.
  */
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '../../../cms-structure/page/page-layout/page-layout.module';
-import * as ɵngcc2 from '@angular/router';
 export declare class LoginRouteModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<LoginRouteModule, never, [typeof ɵngcc1.PageLayoutModule, typeof ɵngcc2.RouterModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<LoginRouteModule>;
 }
-
-//# sourceMappingURL=login-route.module.d.ts.map
