e2abf8cba63e521d163c0d2e39cf138af843e78d
GitHub <noreply@github.com>
2020-12-03 17:52:12 +0000

    14b21330d Fix overlapping text (#10177)

--- cms-components/myaccount/order/amend-order/cancellations/order-cancellation.service.d.ts
+++ cms-components/myaccount/order/amend-order/cancellations/order-cancellation.service.d.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs';
 import { OrderDetailsService } from '../../order-details/order-details.service';
 import { AmendOrderType } from '../amend-order.model';
 import { OrderAmendService } from '../amend-order.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class OrderCancellationService extends OrderAmendService {
     protected orderDetailsService: OrderDetailsService;
     protected userOrderService: UserOrderService;
@@ -17,7 +16,4 @@ export declare class OrderCancellationService extends OrderAmendService {
     getEntries(): Observable<OrderEntry[]>;
     save(): void;
     private afterSave;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<OrderCancellationService, never>;
 }
-
-//# sourceMappingURL=order-cancellation.service.d.ts.map
