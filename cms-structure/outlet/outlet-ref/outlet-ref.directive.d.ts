63396b4df115c47abf4f0de46dd2e00183304870
GitHub <noreply@github.com>
2020-12-04 09:15:03 +0000

    ef1cca500 fix: Drop usage of deprecated makeErrorSerializable (#10215)

--- cms-structure/outlet/outlet-ref/outlet-ref.directive.d.ts
+++ cms-structure/outlet/outlet-ref/outlet-ref.directive.d.ts
@@ -1,6 +1,7 @@
 import { OnDestroy, OnInit, TemplateRef } from '@angular/core';
 import { OutletPosition } from '../outlet.model';
 import { OutletService } from '../outlet.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class OutletRefDirective implements OnInit, OnDestroy {
     private tpl;
     private outletService;
@@ -9,4 +10,8 @@ export declare class OutletRefDirective implements OnInit, OnDestroy {
     constructor(tpl: TemplateRef<any>, outletService: OutletService);
     ngOnInit(): void;
     ngOnDestroy(): void;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<OutletRefDirective, never>;
+    static ɵdir: ɵngcc0.ɵɵDirectiveDefWithMeta<OutletRefDirective, "[cxOutletRef]", never, { "cxOutletRef": "cxOutletRef"; "cxOutletPos": "cxOutletPos"; }, {}, never>;
 }
+
+//# sourceMappingURL=outlet-ref.directive.d.ts.map
--- events/cart/cart-page-event.builder.d.ts
+++ events/cart/cart-page-event.builder.d.ts
@@ -2,10 +2,14 @@ import { ActionsSubject } from '@ngrx/store';
 import { EventService } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { CartPageEvent } from './cart-page.events';
+import * as ɵngcc0 from '@angular/core';
 export declare class CartPageEventBuilder {
     protected actions: ActionsSubject;
     protected eventService: EventService;
     constructor(actions: ActionsSubject, eventService: EventService);
     protected register(): void;
     protected buildCartPageEvent(): Observable<CartPageEvent>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<CartPageEventBuilder, never>;
 }
+
+//# sourceMappingURL=cart-page-event.builder.d.ts.map
