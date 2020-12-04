57b9d98498be517c9cbeb9b9f745d491e927b2c0
GitHub <noreply@github.com>
2020-12-04 10:54:11 +0000

    021820078 feat: allow for providing subtitle in cx-org-card (#10217)

--- cms-components/product/product-variants/guards/product-variant.guard.d.ts
+++ cms-components/product/product-variants/guards/product-variant.guard.d.ts
@@ -1,7 +1,6 @@
 import { ActivatedRouteSnapshot, CanActivate, Router, UrlTree } from '@angular/router';
 import { ProductService, SemanticPathService, VariantOption } from '@spartacus/core';
 import { Observable } from 'rxjs';
-import * as ɵngcc0 from '@angular/core';
 export declare class ProductVariantGuard implements CanActivate {
     protected productService: ProductService;
     protected semanticPathService: SemanticPathService;
@@ -9,7 +8,4 @@ export declare class ProductVariantGuard implements CanActivate {
     constructor(productService: ProductService, semanticPathService: SemanticPathService, router: Router);
     canActivate(activatedRoute: ActivatedRouteSnapshot): Observable<boolean | UrlTree>;
     findVariant(variants: VariantOption[]): VariantOption;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<ProductVariantGuard, never>;
 }
-
-//# sourceMappingURL=product-variant.guard.d.ts.map
