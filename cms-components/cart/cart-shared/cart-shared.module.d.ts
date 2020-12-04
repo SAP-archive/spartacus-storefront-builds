c9e63a4a9891a367bec11bf4d648bce69164520b
GitHub <noreply@github.com>
2020-12-04 18:27:34 +0000

    31ff35192 fix: replace map with forEach (#8823) (#9833)

--- cms-components/cart/cart-shared/cart-shared.module.d.ts
+++ cms-components/cart/cart-shared/cart-shared.module.d.ts
@@ -1,2 +1,20 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './cart-item/cart-item.component';
+import * as ɵngcc2 from './order-summary/order-summary.component';
+import * as ɵngcc3 from './cart-item-list/cart-item-list.component';
+import * as ɵngcc4 from '@angular/common';
+import * as ɵngcc5 from '@angular/router';
+import * as ɵngcc6 from '../cart-coupon/cart-coupon.module';
+import * as ɵngcc7 from '@angular/forms';
+import * as ɵngcc8 from '@spartacus/core';
+import * as ɵngcc9 from '@ng-bootstrap/ng-bootstrap';
+import * as ɵngcc10 from '../../checkout/components/promotions/promotions.module';
+import * as ɵngcc11 from '../../../shared/components/media/media.module';
+import * as ɵngcc12 from '../../../shared/components/item-counter/item-counter.module';
+import * as ɵngcc13 from '../../../shared/components/modal/modal.module';
 export declare class CartSharedModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CartSharedModule, [typeof ɵngcc1.CartItemComponent, typeof ɵngcc2.OrderSummaryComponent, typeof ɵngcc3.CartItemListComponent], [typeof ɵngcc4.CommonModule, typeof ɵngcc5.RouterModule, typeof ɵngcc6.CartCouponModule, typeof ɵngcc7.ReactiveFormsModule, typeof ɵngcc8.UrlModule, typeof ɵngcc9.NgbModule, typeof ɵngcc10.PromotionsModule, typeof ɵngcc8.I18nModule, typeof ɵngcc11.MediaModule, typeof ɵngcc12.ItemCounterModule, typeof ɵngcc8.FeaturesConfigModule, typeof ɵngcc13.ModalModule], [typeof ɵngcc1.CartItemComponent, typeof ɵngcc3.CartItemListComponent, typeof ɵngcc2.OrderSummaryComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<CartSharedModule>;
 }
+
+//# sourceMappingURL=cart-shared.module.d.ts.map
--- cms-components/checkout/guards/shipping-address-set.guard.d.ts
+++ cms-components/checkout/guards/shipping-address-set.guard.d.ts
@@ -3,6 +3,7 @@ import { RoutingConfigService } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { CheckoutStepService } from '../services/checkout-step.service';
 import { CheckoutDetailsService } from '../services/checkout-details.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class ShippingAddressSetGuard implements CanActivate {
     private checkoutDetailsService;
     private routingConfigService;
@@ -10,4 +11,7 @@ export declare class ShippingAddressSetGuard implements CanActivate {
     private checkoutStepService;
     constructor(checkoutDetailsService: CheckoutDetailsService, routingConfigService: RoutingConfigService, router: Router, checkoutStepService: CheckoutStepService);
     canActivate(): Observable<boolean | UrlTree>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<ShippingAddressSetGuard, never>;
 }
+
+//# sourceMappingURL=shipping-address-set.guard.d.ts.map
