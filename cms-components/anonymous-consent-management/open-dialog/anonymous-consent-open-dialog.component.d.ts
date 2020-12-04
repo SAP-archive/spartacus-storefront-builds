2e6dcce32c24716fcf55eb588560ab1cca574d71
GitHub <noreply@github.com>
2020-12-04 13:34:23 +0000

    72bbac00a feat: MyCo features E2E config and user password tests (#10005)

--- cms-components/anonymous-consent-management/open-dialog/anonymous-consent-open-dialog.component.d.ts
+++ cms-components/anonymous-consent-management/open-dialog/anonymous-consent-open-dialog.component.d.ts
@@ -1,14 +1,9 @@
 import { ElementRef, ViewContainerRef } from '@angular/core';
 import { AnonymousConsentLaunchDialogService } from '../anonymous-consent-launch-dialog.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class AnonymousConsentOpenDialogComponent {
     protected vcr: ViewContainerRef;
     protected anonymousConsentLaunchDialogService: AnonymousConsentLaunchDialogService;
     openElement: ElementRef;
     constructor(vcr: ViewContainerRef, anonymousConsentLaunchDialogService: AnonymousConsentLaunchDialogService);
     openDialog(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<AnonymousConsentOpenDialogComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<AnonymousConsentOpenDialogComponent, "cx-anonymous-consent-open-dialog", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=anonymous-consent-open-dialog.component.d.ts.map
--- cms-components/navigation/search-box/highlight.pipe.d.ts.__ivy_ngcc_bak
+++
@@ -1,4 +0,0 @@
-import { PipeTransform } from '@angular/core';
-export declare class HighlightPipe implements PipeTransform {
-    transform(text: string, match?: string): string;
-}
