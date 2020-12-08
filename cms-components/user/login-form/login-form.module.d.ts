3cd84b83dca980243344cf8f0a6c288e91cae0fc
GitHub <noreply@github.com>
2020-12-08 18:52:45 +0000

    c87d9a352 chore: temporarily skip failing b2b regression tests (#10272)

--- cms-components/user/login-form/login-form.module.d.ts
+++ cms-components/user/login-form/login-form.module.d.ts
@@ -1,13 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './login-form.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@angular/forms';
-import * as ɵngcc4 from '@angular/router';
-import * as ɵngcc5 from '@spartacus/core';
-import * as ɵngcc6 from '../../../shared/components/form/form-errors/form-errors.module';
 export declare class LoginFormModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<LoginFormModule, [typeof ɵngcc1.LoginFormComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.FormsModule, typeof ɵngcc3.ReactiveFormsModule, typeof ɵngcc4.RouterModule, typeof ɵngcc5.UrlModule, typeof ɵngcc5.I18nModule, typeof ɵngcc6.FormErrorsModule], [typeof ɵngcc1.LoginFormComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<LoginFormModule>;
 }
-
-//# sourceMappingURL=login-form.module.d.ts.map
