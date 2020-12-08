eebd11e8f1befd5b2216de41496beec107955419
GitHub <noreply@github.com>
2020-12-08 15:54:12 +0000

    767555efe fix: Align storefinder library to the final shape (#10261)

--- cms-components/checkout/components/promotions/promotions.module.d.ts
+++ cms-components/checkout/components/promotions/promotions.module.d.ts
@@ -1,9 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './promotions.component';
-import * as ɵngcc2 from '@angular/common';
 export declare class PromotionsModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<PromotionsModule, [typeof ɵngcc1.PromotionsComponent], [typeof ɵngcc2.CommonModule], [typeof ɵngcc1.PromotionsComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<PromotionsModule>;
 }
-
-//# sourceMappingURL=promotions.module.d.ts.map
