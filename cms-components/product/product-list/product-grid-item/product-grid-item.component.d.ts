58bad31cdbe75a145fe9b576415f2ec3769eaf02
GitHub <noreply@github.com>
2020-12-08 18:39:56 +0000

    c87d9a352 chore: temporarily skip failing b2b regression tests (#10272)

--- cms-components/product/product-list/product-grid-item/product-grid-item.component.d.ts
+++ cms-components/product/product-list/product-grid-item/product-grid-item.component.d.ts
@@ -1,3 +1,8 @@
+import * as ɵngcc0 from '@angular/core';
 export declare class ProductGridItemComponent {
     product: any;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<ProductGridItemComponent, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<ProductGridItemComponent, "cx-product-grid-item", never, { "product": "product"; }, {}, never, never>;
 }
+
+//# sourceMappingURL=product-grid-item.component.d.ts.map
