3782d2abd02f5326d52200fbd9ca75b4d9247531
GitHub <noreply@github.com>
2020-12-08 20:12:38 +0000

    0ec222b5a fix: remove unnecessary variable usage (#9919)

--- cms-components/myaccount/forgot-password/forgot-password.module.d.ts
+++ cms-components/myaccount/forgot-password/forgot-password.module.d.ts
@@ -1,13 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './forgot-password.component';
-import * as ɵngcc2 from '@angular/common';
-import * as ɵngcc3 from '@angular/forms';
-import * as ɵngcc4 from '@angular/router';
-import * as ɵngcc5 from '@spartacus/core';
-import * as ɵngcc6 from '../../../shared/components/form/form-errors/form-errors.module';
 export declare class ForgotPasswordModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ForgotPasswordModule, [typeof ɵngcc1.ForgotPasswordComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.ReactiveFormsModule, typeof ɵngcc4.RouterModule, typeof ɵngcc5.UrlModule, typeof ɵngcc5.I18nModule, typeof ɵngcc6.FormErrorsModule], [typeof ɵngcc1.ForgotPasswordComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<ForgotPasswordModule>;
 }
-
-//# sourceMappingURL=forgot-password.module.d.ts.map
