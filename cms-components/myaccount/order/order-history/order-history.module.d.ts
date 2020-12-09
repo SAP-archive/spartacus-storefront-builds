bfbd14dd782402e223fa2bab703500e4ea065cd5
GitHub <noreply@github.com>
2020-12-09 12:55:25 +0000

    a7b4163be chore: move b2b-account-checkout-flow.e2e-spec.ts from smoke to regression (#10305)

--- cms-components/myaccount/order/order-history/order-history.module.d.ts
+++ cms-components/myaccount/order/order-history/order-history.module.d.ts
@@ -1,2 +1,14 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './order-history.component';
+import * as ɵngcc2 from '@angular/common';
+import * as ɵngcc3 from '@angular/router';
+import * as ɵngcc4 from '@angular/forms';
+import * as ɵngcc5 from '@ng-select/ng-select';
+import * as ɵngcc6 from '../../../../shared/components/list-navigation/list-navigation.module';
+import * as ɵngcc7 from '@spartacus/core';
 export declare class OrderHistoryModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<OrderHistoryModule, [typeof ɵngcc1.OrderHistoryComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.RouterModule, typeof ɵngcc3.RouterModule, typeof ɵngcc4.FormsModule, typeof ɵngcc5.NgSelectModule, typeof ɵngcc6.ListNavigationModule, typeof ɵngcc7.UrlModule, typeof ɵngcc7.I18nModule], [typeof ɵngcc1.OrderHistoryComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<OrderHistoryModule>;
 }
+
+//# sourceMappingURL=order-history.module.d.ts.map
