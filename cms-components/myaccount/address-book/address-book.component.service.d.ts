62d4c8223831a5fa38e5cd9bbd67236af155c390
GitHub <noreply@github.com>
2020-12-10 18:49:03 +0000

    f842a9ae0 fix: Form is frozen while getting error response (e.g. conflict) (general fix) #9351 (#10266)

--- cms-components/myaccount/address-book/address-book.component.service.d.ts
+++ cms-components/myaccount/address-book/address-book.component.service.d.ts
@@ -1,5 +1,6 @@
 import { Address, CheckoutDeliveryService, UserAddressService } from '@spartacus/core';
 import { Observable } from 'rxjs';
+import * as ɵngcc0 from '@angular/core';
 export declare class AddressBookComponentService {
     protected userAddressService: UserAddressService;
     protected checkoutDeliveryService: CheckoutDeliveryService;
@@ -9,4 +10,7 @@ export declare class AddressBookComponentService {
     loadAddresses(): void;
     addUserAddress(address: Address): void;
     updateUserAddress(addressId: string, address: Address): void;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<AddressBookComponentService, never>;
 }
+
+//# sourceMappingURL=address-book.component.service.d.ts.map
--- cms-components/navigation/breadcrumb/breadcrumb.module.d.ts
+++ cms-components/navigation/breadcrumb/breadcrumb.module.d.ts
@@ -1,2 +1,11 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './breadcrumb.component';
+import * as ɵngcc2 from '@angular/common';
+import * as ɵngcc3 from '@angular/router';
+import * as ɵngcc4 from '@spartacus/core';
 export declare class BreadcrumbModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<BreadcrumbModule, [typeof ɵngcc1.BreadcrumbComponent], [typeof ɵngcc2.CommonModule, typeof ɵngcc3.RouterModule, typeof ɵngcc4.CmsPageTitleModule], [typeof ɵngcc1.BreadcrumbComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<BreadcrumbModule>;
 }
+
+//# sourceMappingURL=breadcrumb.module.d.ts.map
