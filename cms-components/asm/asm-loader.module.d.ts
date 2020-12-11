20777e369327465c86387781c77f32555a9e9495
GitHub <noreply@github.com>
2020-12-11 08:26:59 +0000

    ecb603f24 invalidation styling on untouched form controls (#10352)

--- cms-components/asm/asm-loader.module.d.ts
+++ cms-components/asm/asm-loader.module.d.ts
@@ -3,7 +3,12 @@ import { AsmEnablerService } from './services/asm-enabler.service';
  * The ASM loader module takes care of loading the ASM UI
  * only in case there's a reason to do so.
  */
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from '@angular/common';
+import * as ɵngcc2 from '../../cms-structure/page/component/page-component.module';
 export declare class AsmLoaderModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<AsmLoaderModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.PageComponentModule], never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<AsmLoaderModule>;
 }
 /**
  *
@@ -12,3 +17,5 @@ export declare class AsmLoaderModule {
  * way during the APP_INITIALIZER.
  */
 export declare function asmFactory(asmEnablerService: AsmEnablerService): () => void;
+
+//# sourceMappingURL=asm-loader.module.d.ts.map
---
+++ cms-components/myaccount/close-account/components/close-account/close-account.component.d.ts.__ivy_ngcc_bak
@@ -0,0 +1,7 @@
+import { ModalService } from '../../../../../shared/components/modal/index';
+export declare class CloseAccountComponent {
+    private modalService;
+    modal: any;
+    constructor(modalService: ModalService);
+    openModal(): void;
+}
