471e339e2e020be885ead8fc99b5b1213c04e017
GitHub <noreply@github.com>
2020-12-14 14:30:55 +0000

    febb4bdec fix: Fixing broken test (#10389)

--- cms-components/myaccount/order/amend-order/cancellations/order-cancellation.guard.d.ts
+++ cms-components/myaccount/order/amend-order/cancellations/order-cancellation.guard.d.ts
@@ -2,10 +2,14 @@ import { CanActivate, Router, UrlTree } from '@angular/router';
 import { SemanticPathService } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { OrderCancellationService } from './order-cancellation.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class OrderCancellationGuard implements CanActivate {
     protected orderAmendService: OrderCancellationService;
     protected semanticPathService: SemanticPathService;
     protected router: Router;
     constructor(orderAmendService: OrderCancellationService, semanticPathService: SemanticPathService, router: Router);
     canActivate(): Observable<boolean | UrlTree>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<OrderCancellationGuard, never>;
 }
+
+//# sourceMappingURL=order-cancellation.guard.d.ts.map
