10c978729ba3565aa034b18aa8d73f78a27489cd
GitHub <noreply@github.com>
2020-12-14 14:40:16 +0000

    febb4bdec fix: Fixing broken test (#10389)

--- shared/components/modal/modal.module.d.ts
+++ shared/components/modal/modal.module.d.ts
@@ -1,8 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './modal.directive';
 export declare class ModalModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ModalModule, [typeof ɵngcc1.ModalDirective], never, [typeof ɵngcc1.ModalDirective]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<ModalModule>;
 }
-
-//# sourceMappingURL=modal.module.d.ts.map
