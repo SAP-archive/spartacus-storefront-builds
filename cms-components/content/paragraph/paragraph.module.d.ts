ceba4888386af52550f3f7b64f07cfd40c75f1bf
GitHub <noreply@github.com>
2020-12-15 17:04:50 +0000

    d61f97214 fix: Extra job to run pwa schematics (#10426)

--- cms-components/content/paragraph/paragraph.module.d.ts
+++ cms-components/content/paragraph/paragraph.module.d.ts
@@ -1,9 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './paragraph.component';
-import * as ɵngcc2 from '@angular/common';
 export declare class CmsParagraphModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CmsParagraphModule, [typeof ɵngcc1.ParagraphComponent], [typeof ɵngcc2.CommonModule], [typeof ɵngcc1.ParagraphComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<CmsParagraphModule>;
 }
-
-//# sourceMappingURL=paragraph.module.d.ts.map
