db3835e9603f7b4fbcf03cc5a4bb16b8ed88b3a9
GitHub <noreply@github.com>
2020-12-17 12:32:56 +0000

    61f470cee chore: build the CSR installed app in prod mode (#10457)

--- cms-structure/pwa/pwa.module-config.d.ts
+++ cms-structure/pwa/pwa.module-config.d.ts
@@ -1,7 +1,11 @@
+import * as ɵngcc0 from '@angular/core';
 export declare abstract class PWAModuleConfig {
     pwa?: {
         enabled?: boolean;
         addToHomeScreen?: boolean;
     };
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<PWAModuleConfig, never>;
 }
 export declare const defaultPWAModuleConfig: PWAModuleConfig;
+
+//# sourceMappingURL=pwa.module-config.d.ts.map
