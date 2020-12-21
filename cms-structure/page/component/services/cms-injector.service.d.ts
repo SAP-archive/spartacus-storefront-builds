587fd24037a6677aa6fe21e7bb4c413d4b26da92
GitHub <noreply@github.com>
2020-12-21 12:08:37 +0000

    0b7cded0e fix: show global message when no customerId provided on session start (#10506)

--- cms-structure/page/component/services/cms-injector.service.d.ts
+++ cms-structure/page/component/services/cms-injector.service.d.ts
@@ -6,13 +6,9 @@ import { CmsComponentsService } from '../../../services/cms-components.service';
  * Injector will take into account configured providers and provides CmsComponentData
  * for specified component's uid
  */
-import * as ɵngcc0 from '@angular/core';
 export declare class CmsInjectorService {
     protected cmsComponentsService: CmsComponentsService;
     protected injector: Injector;
     constructor(cmsComponentsService: CmsComponentsService, injector: Injector);
     getInjector(type: string, uid: string, parentInjector?: Injector): Injector;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CmsInjectorService, never>;
 }
-
-//# sourceMappingURL=cms-injector.service.d.ts.map
