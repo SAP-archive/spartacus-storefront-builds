9891c3c152ebbaf0fd8300cde992056101946851
GitHub <noreply@github.com>
2021-01-02 12:22:16 +0000

    de2386854 [Android][Chrome] I don't see selected option if typed text previously #10127 (#10513)

--- cms-components/product/carousel/product-carousel/product-carousel.component.d.ts
+++ cms-components/product/carousel/product-carousel/product-carousel.component.d.ts
@@ -1,7 +1,6 @@
 import { CmsProductCarouselComponent as model, Product, ProductScope, ProductService } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { CmsComponentData } from '../../../../cms-structure/page/model/cms-component-data';
-import * as ɵngcc0 from '@angular/core';
 export declare class ProductCarouselComponent {
     protected componentData: CmsComponentData<model>;
     protected productService: ProductService;
@@ -18,8 +17,4 @@ export declare class ProductCarouselComponent {
      */
     items$: Observable<Observable<Product>[]>;
     constructor(componentData: CmsComponentData<model>, productService: ProductService);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<ProductCarouselComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<ProductCarouselComponent, "cx-product-carousel", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=product-carousel.component.d.ts.map
