28d28185055892aa6862aa5954a745666fb417ec
GitHub <noreply@github.com>
2021-01-02 12:14:01 +0000

    de2386854 [Android][Chrome] I don't see selected option if typed text previously #10127 (#10513)

--- shared/components/list-navigation/pagination/config/pagination.config.d.ts
+++ shared/components/list-navigation/pagination/config/pagination.config.d.ts
@@ -1,4 +1,8 @@
 import { PaginationOptions } from '../pagination.model';
+import * as ɵngcc0 from '@angular/core';
 export declare abstract class PaginationConfig {
     pagination?: PaginationOptions;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<PaginationConfig, never>;
 }
+
+//# sourceMappingURL=pagination.config.d.ts.map
