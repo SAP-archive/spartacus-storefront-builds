74c0229e72c460b307251821bd54b1457c9738cc
GitHub <noreply@github.com>
2021-01-07 09:44:27 +0000

    5bd344c90 feat: Unify user roles checkboxes (#10579)

--- cms-components/myaccount/order/order-details/order-detail-items/order-detail-items.component.d.ts
+++ cms-components/myaccount/order/order-details/order-detail-items/order-detail-items.component.d.ts
@@ -3,7 +3,6 @@ import { Consignment, PromotionLocation, PromotionResult } from '@spartacus/core
 import { Observable } from 'rxjs';
 import { PromotionService } from '../../../../../shared/services/promotion/promotion.service';
 import { OrderDetailsService } from '../order-details.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class OrderDetailItemsComponent implements OnInit {
     protected orderDetailsService: OrderDetailsService;
     protected promotionService: PromotionService;
@@ -17,8 +16,4 @@ export declare class OrderDetailItemsComponent implements OnInit {
     ngOnInit(): void;
     private getExactStatus;
     private getOtherStatus;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<OrderDetailItemsComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<OrderDetailItemsComponent, "cx-order-details-items", never, {}, {}, never, never>;
 }
-
-//# sourceMappingURL=order-detail-items.component.d.ts.map
