35b863492ac826527faa6286f12353cb50c82e59
GitHub <noreply@github.com>
2021-01-08 15:17:12 +0000

    5e0e65955 feat: login and logout event (#10627)

--- events/product/product-page-event.module.d.ts
+++ events/product/product-page-event.module.d.ts
@@ -1,9 +1,4 @@
 import { ProductPageEventBuilder } from './product-page-event.builder';
-import * as ɵngcc0 from '@angular/core';
 export declare class ProductPageEventModule {
     constructor(_productPageEventBuilder: ProductPageEventBuilder);
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ProductPageEventModule, never, never, never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<ProductPageEventModule>;
 }
-
-//# sourceMappingURL=product-page-event.module.d.ts.map
